eb958de293c896f4a6826241b3c9bc6ce952b29d
chimp1984 <chimp1984@gmx.com>
2021-06-12 13:35:41 -0500

    Add network module Remove p2p module

--- p2p/src/main/java/network/misq/p2p/P2PService.java
+++
@@ -1,8 +0,0 @@
-package network.misq.p2p;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class P2PService {
-    private static final Logger log = LoggerFactory.getLogger(P2PService.class);
-}
