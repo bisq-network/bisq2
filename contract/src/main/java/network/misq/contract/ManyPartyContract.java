c1c90b420e42997589aacb8d290735a95f62e526
chimp1984 <chimp1984@gmx.com>
2021-06-12 14:39:43 -0500

    Add contract module
    Some preliminary implementation (used in the POC prototype). Added for preparing application structure and defining dependencies

---
+++ contract/src/main/java/network/misq/contract/ManyPartyContract.java
@@ -0,0 +1,17 @@
+package network.misq.contract;
+
+import lombok.Getter;
+
+import javax.management.relation.Role;
+import java.util.Map;
+
+@Getter
+public class ManyPartyContract extends Contract {
+    private final Map<Role, Party> partyMap;
+
+    public ManyPartyContract(ProtocolType protocolType, Role myRole, Map<Role, Party> partyMap) {
+        super(protocolType, myRole);
+
+        this.partyMap = Map.copyOf(partyMap);
+    }
+}
