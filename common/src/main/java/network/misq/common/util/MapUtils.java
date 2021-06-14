4e5f225c97737bef06d71e0a40fae996945bc9f7
chimp1984 <chimp1984@gmx.com>
2021-06-14 09:05:35 -0500

    Fix incorrect map access

--- common/src/main/java/network/misq/common/util/MapUtils.java
+++ common/src/main/java/network/misq/common/util/MapUtils.java
@@ -30,7 +30,7 @@ public class MapUtils {
     }
 
     public static void disposeAndRemoveAll(Map<String, ? extends Disposable> map) {
-        map.keySet().forEach(Disposable::dispose);
+        map.values().forEach(Disposable::dispose);
         map.clear();
     }
 }
