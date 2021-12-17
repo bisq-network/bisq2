85043e333532e4303c62d8429e87ce1bdde72b3f
chimp1984 <chimp1984@gmx.com>
2021-12-17 11:27:01 -0500

    Update to work in progress code. This commit is a series of partial commits for each module. Changes have not been applied to dependent modules yet. So it would not compile.
    Add Persistable
    Implement basic Persistence (work in progress)

--- persistence/src/main/java/network/misq/persistence/Persistence.java
+++ persistence/src/main/java/network/misq/persistence/Persistence.java
@@ -18,9 +18,73 @@
 package network.misq.persistence;
 
 import lombok.extern.slf4j.Slf4j;
+import network.misq.common.threading.ExecutorFactory;
+import network.misq.common.util.FileUtils;
 
 import java.io.*;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 public class Persistence {
+    private final String directory;
+    private final String fileName;
+    private final String storagePath;
+    private final Object writeLock = new Object();
+    private File tempFile;
+    private Serializable serializable;
+
+    public Persistence(String directory, Serializable serializable) {
+        this(directory, serializable.getClass().getSimpleName(), serializable);
+    }
+
+    public Persistence(String directory, String fileName, Serializable serializable) {
+        this.directory = directory;
+        this.serializable = serializable;
+        this.fileName = fileName;
+        storagePath = directory + File.separator + fileName;
+    }
+
+    public static Serializable read(String storagePath) {
+        try (FileInputStream fileInputStream = new FileInputStream(storagePath);
+             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+            return (Serializable) objectInputStream.readObject();
+        } catch (IOException | ClassNotFoundException exception) {
+            log.error(exception.toString(), exception);
+            return null;
+        }
+    }
+
+    public CompletableFuture<Boolean> persist() {
+        return CompletableFuture.supplyAsync(() -> {
+            synchronized (writeLock) {
+                boolean success = false;
+                try {
+                    FileUtils.makeDirs(directory);
+                    // We use a temp file to not risk data corruption in case the write operation fails.
+                    // After write is done we rename the tempFile to our storageFile which is an atomic operation.
+                    tempFile = File.createTempFile("temp_" + fileName, null, new File(directory));
+                    FileUtils.deleteOnExit(tempFile);
+                    File storageFile = new File(directory, fileName);
+                    try (FileOutputStream fileOutputStream = new FileOutputStream(tempFile);
+                         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+                        objectOutputStream.writeObject(serializable);
+                        objectOutputStream.flush();
+                        fileOutputStream.flush();
+                        fileOutputStream.getFD().sync();
+
+                        // Atomic rename
+                        FileUtils.renameFile(tempFile, storageFile);
+                        success = true;
+                    } catch (IOException exception) {
+                        log.error(exception.toString(), exception);
+                    } finally {
+                        FileUtils.releaseTempFile(tempFile);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return success;
+            }
+        }, ExecutorFactory.getSingleThreadExecutor("Write-to-disk: " + fileName));
+    }
 }
