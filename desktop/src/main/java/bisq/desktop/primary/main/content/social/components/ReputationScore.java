eabb279d7143368e9888ace6282e44130c6f3445
chimp1984 <chimp1984@gmx.com>
2022-04-24 23:04:33 +0200

    Add reputation score component

---
+++ desktop/src/main/java/bisq/desktop/primary/main/content/social/components/ReputationScore.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.desktop.primary.main.content.social.components;
+
+import bisq.desktop.common.utils.Icons;
+import bisq.i18n.Res;
+import de.jensd.fx.fontawesome.AwesomeIcon;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.HBox;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class ReputationScore extends HBox {
+    final List<Label> stars;
+    final Tooltip tooltip = new Tooltip();
+
+    public ReputationScore() {
+        tooltip.setStyle("-fx-text-fill: black; -fx-background-color: -bisq-grey-11;");
+        tooltip.setMaxWidth(300);
+        tooltip.setWrapText(true);
+
+        String fontSize = "0.9em";
+        stars = List.of(Icons.getIcon(AwesomeIcon.STAR, fontSize),
+                Icons.getIcon(AwesomeIcon.STAR, fontSize),
+                Icons.getIcon(AwesomeIcon.STAR, fontSize),
+                Icons.getIcon(AwesomeIcon.STAR, fontSize),
+                Icons.getIcon(AwesomeIcon.STAR, fontSize));
+        for (Label label : stars) {
+            label.setMouseTransparent(false);
+        }
+        setSpacing(5);
+        setAlignment(Pos.CENTER);
+        getChildren().addAll(stars);
+    }
+
+    public void applyScores(int ranking, int score, double relativeScore) {
+        int target = (int) Math.floor((stars.size() + 1) * relativeScore) - 1;
+        for (int i = 0; i < stars.size(); i++) {
+            stars.get(i).setOpacity(i <= target ? 1 : 0.1);
+        }
+
+        long percent = Math.round(relativeScore * 10000) / 100;
+        tooltip.setText(Res.get("reputation.score.tooltip",
+                ranking, score, percent));
+        Tooltip.install(this, tooltip);
+    }
+}
