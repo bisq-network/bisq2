de3fe5dff7e9064b1096489f5f5529af2aca0d94
xyzmaker123 <xyzmaker123@protonmail.com>
2022-05-05 12:32:36 +0200

    Welcome screen - redesign - fix headline setup

--- desktop/src/main/java/bisq/desktop/primary/main/content/social/SocialView.java
+++ desktop/src/main/java/bisq/desktop/primary/main/content/social/SocialView.java
@@ -17,18 +17,17 @@
 
 package bisq.desktop.primary.main.content.social;
 
-import bisq.desktop.common.view.*;
-import bisq.desktop.overlay.OverlayWindow;
+import bisq.desktop.common.view.NavigationTarget;
+import bisq.desktop.common.view.TabView;
 import bisq.desktop.primary.main.content.social.gettingStarted.GettingStartedView;
 import bisq.i18n.Res;
-import javafx.beans.value.ChangeListener;
-import javafx.scene.Parent;
-import javafx.scene.layout.Region;
 import lombok.extern.slf4j.Slf4j;
+import org.fxmisc.easybind.EasyBind;
+import org.fxmisc.easybind.Subscription;
 
 @Slf4j
 public class SocialView extends TabView<SocialModel, SocialController> {
-    protected final ChangeListener<View<? extends Parent, ? extends Model, ? extends Controller>> viewChangeListener;
+    private Subscription selectedViewSubscription;
     
     public SocialView(SocialModel model, SocialController controller) {
         super(model, controller);
@@ -37,30 +36,27 @@ public class SocialView extends TabView<SocialModel, SocialController> {
         addTab(Res.get("social.discuss"), NavigationTarget.DISCUSS);
         addTab(Res.get("social.learn"), NavigationTarget.LEARN);
         addTab(Res.get("social.connect"), NavigationTarget.CONNECT);
-
-        setupHeadline(model.getView()); // TODO: check why view is null at the beginning
-        viewChangeListener = (observable, oldValue, newValue) -> setupHeadline(newValue);
     }
 
     @Override
     protected void onViewAttached() {
-        model.getView().addListener(viewChangeListener);
+        selectedViewSubscription = EasyBind.subscribe(model.getView(), view -> {
+            if (view instanceof GettingStartedView) {
+                headlineLabel.setText(Res.get("welcome"));
+                if (!headlineLabel.getStyleClass().contains("super-large-text")) {
+                    headlineLabel.getStyleClass().add("super-large-text");
+                }
+                line.setOpacity(0);
+            } else {
+                headlineLabel.setText(Res.get("social"));
+                headlineLabel.getStyleClass().remove("super-large-text");
+                line.setOpacity(1);
+            }
+        });
     }
 
     @Override
     protected void onViewDetached() {
-        model.getView().removeListener(viewChangeListener);
-    }
-    
-    private void setupHeadline(Object view) {
-        if (view instanceof GettingStartedView) {
-            headlineLabel.setText(Res.get("welcome"));
-            headlineLabel.getStyleClass().add("super-large-text");
-            line.setOpacity(0);
-        } else {
-            headlineLabel.setText(Res.get("social"));
-            headlineLabel.getStyleClass().remove("super-large-text");
-            line.setOpacity(1);
-        }
+        selectedViewSubscription.unsubscribe();
     }
 }
