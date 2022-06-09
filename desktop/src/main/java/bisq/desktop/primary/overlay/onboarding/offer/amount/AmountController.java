c42dd60f71434f815d5b70705a58e28e7f5de925
chimp1984 <chimp1984@gmx.com>
2022-06-09 03:01:38 +0200

    Apply calculation to amount screen

--- desktop/src/main/java/bisq/desktop/primary/overlay/onboarding/offer/amount/AmountController.java
+++ desktop/src/main/java/bisq/desktop/primary/overlay/onboarding/offer/amount/AmountController.java
@@ -18,30 +18,133 @@
 package bisq.desktop.primary.overlay.onboarding.offer.amount;
 
 import bisq.application.DefaultApplicationService;
+import bisq.common.currency.Market;
+import bisq.common.currency.MarketRepository;
+import bisq.common.monetary.Coin;
+import bisq.common.monetary.Monetary;
+import bisq.common.monetary.Quote;
+import bisq.desktop.common.threading.UIThread;
 import bisq.desktop.common.view.Controller;
 import bisq.desktop.common.view.Navigation;
 import bisq.desktop.common.view.NavigationTarget;
-import bisq.offer.spec.Direction;
+import bisq.desktop.components.controls.PriceInput;
+import javafx.beans.value.ChangeListener;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.fxmisc.easybind.EasyBind;
+import org.fxmisc.easybind.Subscription;
 
 @Slf4j
 public class AmountController implements Controller {
     private final AmountModel model;
     @Getter
     private final AmountView view;
+    private final BigAmountInput baseAmount;
+    private final SmallAmountInput quoteAmount;
+    private final ChangeListener<Monetary> baseCurrencyAmountListener, quoteCurrencyAmountListener;
+    private final ChangeListener<Quote> fixPriceQuoteListener;
+    private final PriceInput price;
+    private Subscription sliderAmountSubscription, baseSideAmountSubscription;
 
     public AmountController(DefaultApplicationService applicationService) {
-        model = new AmountModel();
-        view = new AmountView(model, this);
+        baseAmount = new BigAmountInput(true);
+        quoteAmount = new SmallAmountInput(false);
+        price = new PriceInput(applicationService.getMarketPriceService());
+
+        model = new AmountModel(baseAmount.amountProperty(), quoteAmount.amountProperty(), price.fixPriceProperty());
+        view = new AmountView(model, this,
+                baseAmount.getRoot(),
+                quoteAmount.getRoot(),
+                price.getRoot());
+
+        // We delay with runLater to avoid that we get triggered at market change from the component's data changes and
+        // apply the conversion before the other component has processed the market change event.
+        // The order of the event notification is not deterministic. 
+        baseCurrencyAmountListener = (observable, oldValue, newValue) -> {
+            UIThread.runOnNextRenderFrame(this::setQuoteFromBase);
+        };
+        quoteCurrencyAmountListener = (observable, oldValue, newValue) -> {
+            UIThread.runOnNextRenderFrame(this::setBaseFromQuote);
+        };
+        fixPriceQuoteListener = (observable, oldValue, newValue) -> {
+            UIThread.runOnNextRenderFrame(this::applyFixPrice);
+        };
     }
 
     @Override
     public void onActivate() {
+        model.getMinAmount().set(Coin.asBtc(10000));
+        model.getMaxAmount().set(Coin.asBtc(1000000));
+        model.getSliderMin().set(0);
+        model.getSliderMax().set(1);
+
+        model.getBaseSideAmount().addListener(baseCurrencyAmountListener);
+        model.getQuoteSideAmount().addListener(quoteCurrencyAmountListener);
+        model.getFixPrice().addListener(fixPriceQuoteListener);
+
+        Market selectedMarket = MarketRepository.getMajorMarkets().get(0);
+        baseAmount.setSelectedMarket(selectedMarket);
+        quoteAmount.setSelectedMarket(selectedMarket);
+        price.setSelectedMarket(selectedMarket);
+
+        model.getDirection().set("buy");
+
+        long minAmount = model.getMinAmount().get().getValue();
+        long minMaxDiff = model.getMaxAmount().get().getValue() - minAmount;
+
+
+        sliderAmountSubscription = EasyBind.subscribe(model.getSliderValue(), sliderValueAsNumber -> {
+            double sliderValue = (double) sliderValueAsNumber;
+            long value = Math.round(sliderValue * minMaxDiff) + minAmount;
+            Coin amount = Coin.of(value, "BTC");
+            model.getAmount().set(amount);
+            baseAmount.setAmount(amount);
+        });
+
+        baseSideAmountSubscription = EasyBind.subscribe(model.getBaseSideAmount(), amount -> {
+            // Only apply value from component to slider if we have no focus on slider (not used)
+            if (amount != null && !model.getSliderFocus().get()) {
+                double sliderValue = (amount.getValue() - minAmount) / (double) minMaxDiff;
+                model.getSliderValue().set(sliderValue);
+            }
+        });
+
+        baseAmount.setAmount(Coin.asBtc(330000));
     }
 
     @Override
     public void onDeactivate() {
+        model.getBaseSideAmount().removeListener(baseCurrencyAmountListener);
+        model.getQuoteSideAmount().removeListener(quoteCurrencyAmountListener);
+        model.getFixPrice().removeListener(fixPriceQuoteListener);
+        sliderAmountSubscription.unsubscribe();
+        baseSideAmountSubscription.unsubscribe();
+    }
+
+    private void setQuoteFromBase() {
+        Quote fixPrice = model.getFixPrice().get();
+        if (fixPrice == null) return;
+        Monetary baseCurrencyAmount = model.getBaseSideAmount().get();
+        if (baseCurrencyAmount == null) return;
+        if (fixPrice.getBaseMonetary().getClass() != baseCurrencyAmount.getClass()) return;
+        quoteAmount.setAmount(fixPrice.toQuoteMonetary(baseCurrencyAmount));
+    }
+
+    private void setBaseFromQuote() {
+        Quote fixPrice = model.getFixPrice().get();
+        if (fixPrice == null) return;
+        Monetary quoteCurrencyAmount = model.getQuoteSideAmount().get();
+        if (quoteCurrencyAmount == null) return;
+        if (fixPrice.getQuoteMonetary().getClass() != quoteCurrencyAmount.getClass()) return;
+        baseAmount.setAmount(fixPrice.toBaseMonetary(quoteCurrencyAmount));
+    }
+
+    private void applyFixPrice() {
+        if (model.getBaseSideAmount() == null) {
+            setBaseFromQuote();
+        } else {
+            setQuoteFromBase();
+        }
     }
 
     public void onNext() {
--- desktop/src/main/java/bisq/desktop/primary/overlay/onboarding/offer/amount/SmallAmountInput.java
+++ desktop/src/main/java/bisq/desktop/primary/overlay/onboarding/offer/amount/SmallAmountInput.java
@@ -15,28 +15,33 @@
  * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bisq.desktop.components.controls;
+package bisq.desktop.primary.overlay.onboarding.offer.amount;
 
 import bisq.common.currency.Market;
 import bisq.common.monetary.Monetary;
 import bisq.desktop.common.utils.validation.MonetaryValidator;
+import bisq.desktop.components.controls.BisqIconButton;
 import bisq.i18n.Res;
-import bisq.offer.spec.Direction;
 import bisq.presentation.formatters.AmountFormatter;
 import bisq.presentation.parser.AmountParser;
 import javafx.beans.property.*;
 import javafx.beans.value.ChangeListener;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class AmountInput {
+public class SmallAmountInput {
     private final Controller controller;
 
-    public AmountInput(boolean isBaseCurrency) {
+    public SmallAmountInput(boolean isBaseCurrency) {
         controller = new Controller(isBaseCurrency);
     }
 
@@ -48,16 +53,8 @@ public class AmountInput {
         controller.setSelectedMarket(selectedMarket);
     }
 
-    public void setDirection(Direction direction) {
-        controller.setDirection(direction);
-    }
-
     public void setAmount(Monetary value) {
         controller.model.amount.set(value);
-    }
-
-    public void setIsTakeOffer() {
-        controller.model.isCreateOffer = false;
     }
 
     public Pane getRoot() {
@@ -81,16 +78,9 @@ public class AmountInput {
             updateModel();
         }
 
-        private void setDirection(Direction direction) {
-            model.direction = direction;
-            updateModel();
-        }
-
         @Override
         public void onActivate() {
-            if (model.isCreateOffer) {
-                model.amount.set(null);
-            }
+            model.amount.set(null);
             updateModel();
         }
 
@@ -116,80 +106,70 @@ public class AmountInput {
             }
             if (model.code.get() == null) return;
             model.amount.set(AmountParser.parse(value, model.code.get()));
-
         }
 
         private void updateModel() {
             if (model.selectedMarket == null) {
                 model.code.set("");
-                model.prompt.set("");
-                model.description.set("");
                 return;
             }
 
             model.code.set(model.isBaseCurrency ? model.selectedMarket.baseCurrencyCode() : model.selectedMarket.quoteCurrencyCode());
-            String code = model.code.get();
-            model.prompt.set(Res.get("createOffer.volume.prompt", code));
-            String dir;
-            if (model.isBaseCurrency) {
-                dir = model.direction == Direction.BUY ? Res.get("buy") : Res.get("sell");
-            } else {
-                dir = model.direction == Direction.BUY ? Res.get("spend") : Res.get("receive");
-            }
-            model.description.set(Res.get("createOffer.amount.description", code, dir));
         }
     }
 
     private static class Model implements bisq.desktop.common.view.Model {
         private final ObjectProperty<Monetary> amount = new SimpleObjectProperty<>();
         private final boolean isBaseCurrency;
-        private final StringProperty description = new SimpleStringProperty();
-        private final StringProperty prompt = new SimpleStringProperty();
         private final StringProperty code = new SimpleStringProperty();
         private Market selectedMarket;
-        private Direction direction;
         public boolean hasFocus;
-        private boolean isCreateOffer = true;
 
         private Model(boolean isBaseCurrency) {
             this.isBaseCurrency = isBaseCurrency;
         }
     }
 
-    public static class View extends bisq.desktop.common.view.View<Pane, Model, Controller> {
-        private final static int WIDTH = 250;
-        private final static int CODE_LABEL_WIDTH = 60;
+    public static class View extends bisq.desktop.common.view.View<HBox, Model, Controller> {
+        private static final String PREFIX = "~ ";
         private final ChangeListener<String> textInputListener;
         private final ChangeListener<Boolean> focusListener;
         private final ChangeListener<Monetary> amountListener;
-        private final Label rightLabel;
-        private final TextInputBox textInputBox;
+        private final TextField textInput;
+        private final Label codeLabel;
 
         private View(Model model, Controller controller, MonetaryValidator validator) {
-            super(new Pane(), model, controller);
+            super(new HBox(), model, controller);
 
-            textInputBox = new TextInputBox(Res.get("satoshisquareapp.createOffer.maxAmount"),
-                    Res.get("createProfile.nickName.prompt"));
-            textInputBox.setPrefWidth(WIDTH);
-            textInputBox.setValidator(validator);
+            root.setAlignment(Pos.CENTER);
+            root.setSpacing(3);
 
-            rightLabel = new Label();
-            rightLabel.setMinHeight(42);
-            rightLabel.setAlignment(Pos.CENTER_RIGHT);
-            rightLabel.setMinWidth(CODE_LABEL_WIDTH);
-            rightLabel.setMaxWidth(CODE_LABEL_WIDTH);
-            rightLabel.setLayoutX(WIDTH - CODE_LABEL_WIDTH - 13);
-            rightLabel.setLayoutY(11);
-            rightLabel.getStyleClass().add("bisq-amount-input-code-label");
+            textInput = new TextField();
+            textInput.setPrefWidth(100);
+            textInput.setId("quote-amount-text-field");
+            textInput.setAlignment(Pos.CENTER_RIGHT);
+            textInput.setPadding(new Insets(0, 0, 0, 0));
 
-            root.getChildren().addAll(textInputBox, rightLabel);
+            codeLabel = new Label();
+            codeLabel.setAlignment(Pos.CENTER_LEFT);
+            codeLabel.setId("quote-amount-text-field");
+            codeLabel.setPadding(new Insets(0, 0, 0, 0));
+
+            Button marketValueInfo = BisqIconButton.createIconButton("info-circle-solid");
+            Tooltip tooltip = new Tooltip(Res.get("onboarding.amount.marketValueInfo"));
+            tooltip.getStyleClass().add("dark-tooltip");
+            marketValueInfo.setTooltip(tooltip);
+
+            HBox.setMargin(textInput, new Insets(0, 0, 0, -70));
+            HBox.setMargin(marketValueInfo, new Insets(2, 0, 0, 0));
+            root.getChildren().addAll(textInput, codeLabel, marketValueInfo);
 
             //  Listeners on view component events
             focusListener = (o, old, newValue) -> {
                 controller.onFocusChange(newValue);
-                controller.onAmount(textInputBox.getText());
+                controller.onAmount(textInput.getText());
             };
-            textInputListener = (o, old, newValue) -> controller.onAmount(textInputBox.getText());
+            textInputListener = (o, old, newValue) -> controller.onAmount(textInput.getText().replace(PREFIX, ""));
 
             // Listeners on model change
             amountListener = (o, old, newValue) -> applyAmount(newValue);
@@ -197,35 +177,25 @@ public class AmountInput {
 
         @Override
         protected void onViewAttached() {
-            if (model.isCreateOffer) {
-                textInputBox.textProperty().addListener(textInputListener);
-                textInputBox.inputTextFieldFocusedProperty().addListener(focusListener);
-            } else {
-                // editable/disable changes style. setMouseTransparent is just for prototyping now
-                textInputBox.setMouseTransparent(true);
-            }
-            textInputBox.promptTextProperty().bind(model.prompt);
-            textInputBox.descriptionProperty().bind(model.description);
-            rightLabel.textProperty().bind(model.code);
+            textInput.textProperty().addListener(textInputListener);
+            textInput.focusedProperty().addListener(focusListener);
+            codeLabel.textProperty().bind(model.code);
             model.amount.addListener(amountListener);
             applyAmount(model.amount.get());
         }
 
         @Override
         protected void onViewDetached() {
-            if (model.isCreateOffer) {
-                textInputBox.textProperty().removeListener(textInputListener);
-                textInputBox.inputTextFieldFocusedProperty().removeListener(focusListener);
-            }
-            textInputBox.promptTextProperty().unbind();
-            textInputBox.descriptionProperty().unbind();
+            textInput.textProperty().removeListener(textInputListener);
+            textInput.focusedProperty().removeListener(focusListener);
 
-            rightLabel.textProperty().unbind();
+            codeLabel.textProperty().unbind();
             model.amount.removeListener(amountListener);
         }
 
         private void applyAmount(Monetary newValue) {
-            textInputBox.setText(newValue == null ? "" : AmountFormatter.formatAmount(newValue, true));
+            textInput.setText(newValue == null ? "" : 
+                    PREFIX + AmountFormatter.formatAmount(newValue, true));
         }
     }
 }
