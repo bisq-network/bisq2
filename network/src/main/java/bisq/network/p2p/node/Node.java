2fc42274468be9492e00feaeed032a26f4397cf3
chimp1984 <chimp1984@gmx.com>
2022-07-01 12:31:33 +0200

    Add thread number to thread name Add log for node state

--- network/src/main/java/bisq/network/p2p/node/Node.java
+++ network/src/main/java/bisq/network/p2p/node/Node.java
@@ -582,6 +582,7 @@ public class Node implements Connection.Handler {
     }
 
     private void setState(State newState) {
+        log.info("Set new state {} for nodeId {}", newState, nodeId);
         checkArgument(newState.ordinal() > state.get().ordinal(),
                 "New state %s must have a higher ordinal as the current state %s", newState, state.get());
         state.set(newState);
