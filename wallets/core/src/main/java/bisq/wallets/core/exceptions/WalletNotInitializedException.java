91f1b8093e1fb59c6b5ccc4c4b9b44fc4f1a0f96
Alva Swanson <alvasw@protonmail.com>
2023-04-08 07:57:15 +0200

    WalletNotInitializedException: Remove unused constructors

--- wallets/core/src/main/java/bisq/wallets/core/exceptions/WalletNotInitializedException.java
+++ wallets/core/src/main/java/bisq/wallets/core/exceptions/WalletNotInitializedException.java
@@ -18,15 +18,7 @@
 package bisq.wallets.core.exceptions;
 
 public class WalletNotInitializedException extends RuntimeException {
-    public WalletNotInitializedException() {
-        super();
-    }
-
     public WalletNotInitializedException(String message) {
         super(message);
-    }
-
-    public WalletNotInitializedException(String message, Throwable cause) {
-        super(message, cause);
     }
 }
