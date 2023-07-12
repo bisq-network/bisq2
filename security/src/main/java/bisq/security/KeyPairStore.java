05e4bfbeff71d37efdac878c4cdeab82a8aa625e
HenrikJannsen <boilingfrog@gmx.com>
2023-07-12 13:17:53 +0200

    Use a random default keyId
    The default networkId is using the default key ID. It usually is not used for sending private mailbox messages, but in case it would we would have used the "default" value for it and all nodes would try to decrypt the encrypted message (by looking up the key with that id).
    To avoid that, we use now a random keyId prefixed with "default_" created at first start and persisted.

--- security/src/main/java/bisq/security/KeyPairStore.java
+++ security/src/main/java/bisq/security/KeyPairStore.java
@@ -19,9 +19,9 @@ package bisq.security;
 
 import bisq.common.proto.ProtoResolver;
 import bisq.common.proto.UnresolvableProtobufMessageException;
+import bisq.common.util.StringUtils;
 import bisq.persistence.PersistableStore;
 import com.google.protobuf.InvalidProtocolBufferException;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.security.KeyPair;
@@ -32,31 +32,33 @@ import java.util.stream.Collectors;
 
 @Slf4j
 public final class KeyPairStore implements PersistableStore<KeyPairStore> {
-    @Getter
+    private String defaultKeyId = "default_" + StringUtils.createUid();
     private final Map<String, KeyPair> keyPairsById = new ConcurrentHashMap<>();
 
     public KeyPairStore() {
     }
 
-    private KeyPairStore(Map<String, KeyPair> map) {
+    private KeyPairStore(String defaultKeyId, Map<String, KeyPair> map) {
+        this.defaultKeyId = defaultKeyId;
         this.keyPairsById.putAll(map);
     }
 
     @Override
     public KeyPairStore getClone() {
-        return new KeyPairStore(keyPairsById);
+        return new KeyPairStore(defaultKeyId, keyPairsById);
     }
 
     @Override
     public bisq.security.protobuf.KeyPairStore toProto() {
-        return bisq.security.protobuf.KeyPairStore.newBuilder().putAllKeyPairsById(keyPairsById.entrySet().stream()
+        return bisq.security.protobuf.KeyPairStore.newBuilder().setDefaultKeyId(defaultKeyId).putAllKeyPairsById(keyPairsById.entrySet().stream()
                         .collect(Collectors.toMap(Map.Entry::getKey, entry -> KeyPairProtoUtil.toProto(entry.getValue()))))
                 .build();
     }
 
     public static KeyPairStore fromProto(bisq.security.protobuf.KeyPairStore proto) {
-        return new KeyPairStore(proto.getKeyPairsByIdMap().entrySet().stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, e -> KeyPairProtoUtil.fromProto(e.getValue()))));
+        return new KeyPairStore(proto.getDefaultKeyId(),
+                proto.getKeyPairsByIdMap().entrySet().stream()
+                        .collect(Collectors.toMap(Map.Entry::getKey, e -> KeyPairProtoUtil.fromProto(e.getValue()))));
     }
 
     @Override
@@ -72,8 +74,9 @@ public final class KeyPairStore implements PersistableStore<KeyPairStore> {
 
     @Override
     public void applyPersisted(KeyPairStore persisted) {
+        defaultKeyId = persisted.defaultKeyId;
         keyPairsById.clear();
-        keyPairsById.putAll(persisted.getKeyPairsById());
+        keyPairsById.putAll(persisted.keyPairsById);
     }
 
     public Optional<KeyPair> findKeyPair(String keyId) {
@@ -83,4 +86,13 @@ public final class KeyPairStore implements PersistableStore<KeyPairStore> {
     public void put(String keyId, KeyPair keyPair) {
         keyPairsById.put(keyId, keyPair);
     }
+
+    String getDefaultKeyId() {
+        return defaultKeyId;
+    }
+
+    Map<String, KeyPair> getKeyPairsById() {
+        return keyPairsById;
+    }
+
 }
