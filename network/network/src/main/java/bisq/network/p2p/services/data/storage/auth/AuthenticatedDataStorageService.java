d06796f176139b924015e11a034d131a23c29be7
HenrikJannsen <boilingfrog@gmx.com>
2023-08-01 16:09:58 +0200

    Add check if metaData of RemoveAuthenticatedDataRequest is same as AddAuthenticatedDataRequest from the map (if exists)

--- network/network/src/main/java/bisq/network/p2p/services/data/storage/auth/AuthenticatedDataStorageService.java
+++ network/network/src/main/java/bisq/network/p2p/services/data/storage/auth/AuthenticatedDataStorageService.java
@@ -169,6 +169,16 @@ public class AuthenticatedDataStorageService extends DataStorageService<Authenti
             checkArgument(requestFromMap instanceof AddAuthenticatedDataRequest,
                     "requestFromMap expected be type of AddProtectedDataRequest");
             AddAuthenticatedDataRequest addRequestFromMap = (AddAuthenticatedDataRequest) requestFromMap;
+
+            // The metaData provided in the RemoveAuthenticatedDataRequest must be the same as we had in the AddAuthenticatedDataRequest
+            // The AddAuthenticatedDataRequest does use the metaData from the code base, not one provided by the message, thus it is trusted.
+            if (!request.getMetaData().equals(addRequestFromMap.getAuthenticatedSequentialData().getAuthenticatedData().getMetaData())) {
+                log.warn("MetaData of remove request not matching the one from the addRequest from the map. {} vs. {}",
+                        request.getMetaData(),
+                        addRequestFromMap.getAuthenticatedSequentialData().getAuthenticatedData().getMetaData());
+                return new Result(false).metaDataInvalid();
+            }
+
             // We have an entry, lets validate if we can remove it
             AuthenticatedSequentialData dataFromMap = addRequestFromMap.getAuthenticatedSequentialData();
             authenticatedDataFromMap = dataFromMap.getAuthenticatedData();
