ac861eec4fc6320f5f5b5d5876f8852ec3dc2426
namloan <nam_loan@protonmail.com>
2023-08-07 20:10:45 +0200

    Add National Bank payment method

---
+++ account/src/main/java/bisq/account/accounts/NationalBankAccount.java
@@ -0,0 +1,37 @@
+package bisq.account.accounts;
+
+import bisq.account.payment_method.FiatPaymentMethod;
+import bisq.account.payment_method.FiatPaymentRail;
+import bisq.common.locale.Country;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Slf4j
+@ToString
+@EqualsAndHashCode(callSuper = true)
+public class NationalBankAccount extends BankAccount<NationalBankAccountPayload> {
+    private static final FiatPaymentMethod PAYMENT_METHOD = FiatPaymentMethod.fromPaymentRail(FiatPaymentRail.NATIONAL_BANK);
+
+    public NationalBankAccount(String accountName, NationalBankAccountPayload payload, Country country) {
+        super(accountName, PAYMENT_METHOD, payload, country);
+    }
+
+    @Override
+    public bisq.account.protobuf.Account toProto() {
+        return getAccountBuilder()
+                .setCountryBasedAccount(getCountryBasedAccountBuilder()
+                        .setBankAccount(getBankAccountBuilder().setNationalBankAccount(
+                                bisq.account.protobuf.NationalBankAccount.newBuilder())))
+                .build();
+    }
+
+    public static NationalBankAccount fromProto(bisq.account.protobuf.Account proto) {
+        return new NationalBankAccount(
+                proto.getAccountName(),
+                NationalBankAccountPayload.fromProto(proto.getAccountPayload()),
+                Country.fromProto(proto.getCountryBasedAccount().getCountry()));
+    }
+}
