ca94ed6af341963c00b382689a3b358823b2776d
Alva Swanson <alvasw@protonmail.com>
2023-08-18 11:56:19 +0200

    Implement TorrcClientConfigFactory

---
+++ network/tor/tor/src/main/java/bisq/tor/TorrcClientConfigFactory.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.tor;
+
+import bisq.network.tor.common.torrc.BaseTorrcGenerator;
+import bisq.network.tor.common.torrc.ClientTorrcGenerator;
+import bisq.network.tor.common.torrc.TorrcConfigGenerator;
+import lombok.Builder;
+import net.freehaven.tor.control.PasswordDigest;
+
+import java.nio.file.Path;
+import java.util.Map;
+
+@Builder
+public class TorrcClientConfigFactory {
+    public static final String DISABLE_NETWORK_CONFIG_KEY = "DisableNetwork";
+
+    private final Path dataDir;
+    private final int controlPort;
+    private final int socksPort;
+    private final PasswordDigest hashedControlPassword;
+
+    public TorrcClientConfigFactory(Path dataDir, int controlPort, int socksPort, PasswordDigest hashedControlPassword) {
+        this.dataDir = dataDir;
+        this.controlPort = controlPort;
+        this.socksPort = socksPort;
+        this.hashedControlPassword = hashedControlPassword;
+    }
+
+    public Map<String, String> torrcClientConfigMap(Map<String, String> torrcOverrides) {
+        Map<String, String> torrcClientConfig = clientTorrcGenerator().generate();
+        torrcClientConfig.putAll(torrcOverrides);
+        torrcClientConfig.put(DISABLE_NETWORK_CONFIG_KEY, "1");
+        return torrcClientConfig;
+    }
+
+    private TorrcConfigGenerator clientTorrcGenerator() {
+        return ClientTorrcGenerator.builder()
+                .baseTorrcConfigGenerator(baseTorrcGenerator())
+                .socksPort(socksPort)
+                .build();
+    }
+
+    private TorrcConfigGenerator baseTorrcGenerator() {
+        return BaseTorrcGenerator.builder()
+                .dataDirPath(dataDir)
+                .controlPort(controlPort)
+                .hashedControlPassword(hashedControlPassword.getHashedPassword())
+                .build();
+    }
+}
