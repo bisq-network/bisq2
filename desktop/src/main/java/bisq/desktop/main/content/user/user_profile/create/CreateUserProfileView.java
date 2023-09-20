160672881e71f22e3eafcaf2a1d0e5a551a73c04
aspire2dev <aspire2dev@proton.me>
2023-09-20 07:39:08 -0400

    Adding close button to the create new profile screen Resolves bisq-network/bisq2#1196

--- desktop/src/main/java/bisq/desktop/main/content/user/user_profile/create/CreateUserProfileView.java
+++ desktop/src/main/java/bisq/desktop/main/content/user/user_profile/create/CreateUserProfileView.java
@@ -19,23 +19,43 @@ package bisq.desktop.main.content.user.user_profile.create;
 
 import bisq.desktop.common.Transitions;
 import bisq.desktop.common.view.NavigationView;
+import bisq.desktop.components.containers.Spacer;
+import bisq.desktop.components.controls.BisqIconButton;
 import bisq.desktop.overlay.OverlayModel;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CreateUserProfileView extends NavigationView<VBox, CreateUserProfileModel, CreateUserProfileController> {
+
+    public static final double TOP_PANE_HEIGHT = 55;
+    private final Button closeButton;
+
     public CreateUserProfileView(CreateUserProfileModel model, CreateUserProfileController controller) {
         super(new VBox(), model, controller);
 
         root.setPrefWidth(OverlayModel.WIDTH);
         root.setPrefHeight(OverlayModel.HEIGHT);
 
+        closeButton = BisqIconButton.createIconButton("close");
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setMinHeight(TOP_PANE_HEIGHT);
+        hBox.setMaxHeight(TOP_PANE_HEIGHT);
+        hBox.setPadding(new Insets(0, 20, 0, 50));
+        hBox.getChildren().addAll(Spacer.fillHBox(),
+                closeButton);
+
         model.getView().addListener((observable, oldValue, newValue) -> {
             Region childRoot = newValue.getRoot();
             childRoot.setPrefHeight(root.getHeight());
-            root.getChildren().add(childRoot);
+            root.getChildren().addAll(hBox, childRoot);
             if (oldValue != null) {
                 Transitions.transitLeftOut(childRoot, oldValue.getRoot());
             } else {
@@ -46,9 +66,11 @@ public class CreateUserProfileView extends NavigationView<VBox, CreateUserProfil
 
     @Override
     protected void onViewAttached() {
+        closeButton.setOnAction(e -> controller.onClose());
     }
 
     @Override
     protected void onViewDetached() {
+        closeButton.setOnAction(null);
     }
 }
