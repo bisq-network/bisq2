55ffb5d8bfa986f5990ab566bff3599a09f995a2
djing chan <djing-chan-8833@tuta.io>
2023-11-11 14:51:46 +0700

    Refactor: Move package one level up

--- desktop/src/main/java/bisq/desktop/common/standby/PreventStandbyMode.java
+++ desktop/src/main/java/bisq/desktop/common/standby/PreventStandbyMode.java
@@ -15,7 +15,7 @@
  * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bisq.desktop.common.utils.standby;
+package bisq.desktop.common.standby;
 
 interface PreventStandbyMode {
     void initialize();
