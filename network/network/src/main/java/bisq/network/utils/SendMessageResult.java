cba56ec94dc01330e35ac95096b5df9f239c1eb2
djing chan <djing-chan-8833@tuta.io>
2023-11-11 14:45:55 +0700

    Refactor: Move SendMessageResultUtil.findAnyErrorMsg to SendMessageResult.findAnyErrorMsg. Remove SendMessageResultUtil

--- network/network/src/main/java/bisq/network/utils/SendMessageResult.java
+++ network/network/src/main/java/bisq/network/utils/SendMessageResult.java
@@ -4,9 +4,17 @@ import bisq.network.common.TransportType;
 import bisq.network.p2p.services.confidential.ConfidentialMessageService;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 public class SendMessageResult extends HashMap<TransportType, ConfidentialMessageService.Result> {
     public SendMessageResult() {
         super();
+    }
+
+    public static Optional<String> findAnyErrorMsg(SendMessageResult result) {
+        return result.entrySet().stream()
+                .filter(e -> e.getValue().getErrorMsg().isPresent())
+                .map(e -> e.getKey().name() + ": " + e.getValue().getErrorMsg().get())
+                .findAny();
     }
 }
