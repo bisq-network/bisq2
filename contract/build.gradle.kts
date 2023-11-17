865e1833c5e8751b2959f1fedb1ba7eca2419571
Alva Swanson <alvasw@protonmail.com>
2023-11-17 21:11:57 +0100

    Migrate contract module to kotlin-dsl

---
+++ contract/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("bisq.java-library")
+    id("bisq.protobuf")
+}
+
+dependencies {
+    implementation(project(":persistence"))
+    implementation(project(":identity"))
+    implementation(project(":user"))
+    implementation(project(":security"))
+    implementation(project(":account"))
+    implementation(project(":offer"))
+
+    implementation("network:network")
+    implementation("network:network-identity")
+
+    implementation(libs.google.guava)
+}
