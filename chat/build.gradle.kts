7404eae3f642ef8112fea0cacbf1cc215052ba8c
Alva Swanson <alvasw@protonmail.com>
2023-11-17 21:11:18 +0100

    Migrate chat module to kotlin-dsl

---
+++ chat/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("bisq.java-library")
+    id("bisq.protobuf")
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://jitpack.io")
+    }
+}
+
+dependencies {
+    implementation(project(":i18n"))
+    implementation(project(":persistence"))
+    implementation(project(":security"))
+    implementation(project(":identity"))
+    implementation(project(":user"))
+    implementation(project(":offer"))
+    implementation(project(":settings"))
+    implementation(project(":presentation"))
+
+    implementation("network:network")
+    implementation("network:network-identity")
+
+    implementation(libs.chimp.jsocks)
+    implementation(libs.google.gson)
+    implementation(libs.google.guava)
+    implementation(libs.typesafe.config)
+}
