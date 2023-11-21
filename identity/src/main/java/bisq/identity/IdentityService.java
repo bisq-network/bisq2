c2d27ca8428873075c2ee04d5a244599bd34fea1
djing chan <djing-chan-8833@tuta.io>
2023-11-21 17:40:49 +0700

    Add isDefaultIdentity method Rename nodeId to identityTag

--- identity/src/main/java/bisq/identity/IdentityService.java
+++ identity/src/main/java/bisq/identity/IdentityService.java
@@ -259,6 +259,10 @@ public class IdentityService implements PersistenceClient<IdentityStore>, Servic
         return createIdentity(keyId, identityTag, keyPair);
     }
 
+    public boolean isDefaultIdentity(String keyId) {
+        return keyPairService.isDefaultKeyId(keyId);
+    }
+
     private Identity createIdentity(String keyId, String identityTag, KeyPair keyPair) {
         PubKey pubKey = new PubKey(keyPair.getPublic(), keyId);
         boolean isDefaultIdentity = identityTag.equals(DEFAULT_IDENTITY_TAG);
@@ -382,7 +386,7 @@ public class IdentityService implements PersistenceClient<IdentityStore>, Servic
         }
     }
 
-    private String getTorHiddenServiceDirectory(String nodeId) {
-        return Path.of(baseDir, "tor", "hiddenservice", nodeId).toString();
+    private String getTorHiddenServiceDirectory(String identityTag) {
+        return Path.of(baseDir, "tor", "hiddenservice", identityTag).toString();
     }
 }
