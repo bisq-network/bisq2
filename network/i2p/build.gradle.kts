3102c5d30541dd790a04d28ce2b9a31bc73ccfe2
Alva Swanson <alvasw@protonmail.com>
2023-11-21 14:17:52 +0100

    Migrate network-i2p module to kotlin-dsl

--- network/i2p/build.gradle.kts
+++ network/i2p/build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
-    id 'bisq.java-library'
+    id("bisq.java-library")
 }
 
 dependencies {
-    implementation libs.google.guava
+    implementation(libs.google.guava)
 
     // Exclude httpclient transitive dependency because it may override
     // I2P-specific impl of some Apache classes like org.apache.http.util.Args
     implementation(libs.apache.httpcomponents.core) {
-        exclude group: 'org.apache.httpcomponents', module: 'httpclient'
+        exclude(group = "org.apache.httpcomponents", module = "httpclient")
     }
 
-    implementation libs.bundles.i2p
+    implementation(libs.bundles.i2p)
 }
