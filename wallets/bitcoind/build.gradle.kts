a524f8f9714ca457abb4c1f81f2793daca987b7a
Alva Swanson <alvasw@protonmail.com>
2023-11-23 08:09:16 +0100

    Migrate wallets-bitcoind module to kotlin-dsl

---
+++ wallets/bitcoind/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("bisq.java-library")
+    id("bisq.java-integration-tests")
+    id("bisq.protobuf")
+}
+
+dependencies {
+    implementation("bisq:persistence")
+
+    implementation(project(":core"))
+    implementation(project(":json-rpc"))
+
+    implementation(libs.typesafe.config)
+    implementation(libs.google.guava)
+    implementation(libs.jeromq)
+
+    integrationTestImplementation(project(":regtest"))
+}
