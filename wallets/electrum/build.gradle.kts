25bd9cbad958075790803bc029a436e3ed3c6dc6
Alva Swanson <alvasw@protonmail.com>
2023-11-23 08:10:04 +0100

    Migrate wallets-electrum module to kotlin-dsl

---
+++ wallets/electrum/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("bisq.java-library")
+    id("bisq.protobuf")
+    id("bisq.gradle.electrum.BisqElectrumPlugin")
+    id("bisq.java-integration-tests")
+}
+
+electrum {
+    version.set("4.2.2")
+}
+
+sourceSets {
+    main {
+        resources {
+            srcDir(layout.buildDirectory.file("generated/src/main/resources"))
+        }
+    }
+}
+
+dependencies {
+    api(project(":core"))
+
+    implementation("bisq:persistence")
+    implementation(project(":json-rpc"))
+    implementation(project(":process"))
+    
+    implementation(libs.typesafe.config)
+    implementation(libs.bundles.glassfish.jersey)
+
+    integrationTestAnnotationProcessor(libs.lombok)
+    integrationTestCompileOnly(libs.lombok)
+    integrationTestImplementation(project(":bitcoind"))
+    integrationTestImplementation(project(":regtest"))
+}
