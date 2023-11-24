a41d2ce982cb0f8c57f05545dcdb831919d77bc7
Alva Swanson <alvasw@protonmail.com>
2023-11-24 09:40:57 +0100

    Migrate seednode to kotlin-dsl

---
+++ seed_node_app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("bisq.java-library")
+    application
+    alias(libs.plugins.shadow)
+}
+
+application {
+    mainClass.set("bisq.seed_node.SeedNodeApp")
+}
+
+dependencies {
+    implementation(project(":persistence"))
+    implementation(project(":security"))
+    implementation(project(":bonded_roles"))
+    implementation(project(":application"))
+    implementation(project(":identity"))
+
+    implementation("network:common")
+    implementation("network:network")
+    implementation("network:network-identity")
+
+    implementation(libs.typesafe.config)
+    implementation(libs.google.gson)
+}
+
+tasks {
+    distZip {
+        enabled = false
+    }
+
+    distTar {
+        enabled = false
+    }
+
+    shadowDistZip {
+        enabled = false
+    }
+
+    shadowDistTar {
+        enabled = false
+    }
+
+    shadowJar {
+        enabled = false
+    }
+}
