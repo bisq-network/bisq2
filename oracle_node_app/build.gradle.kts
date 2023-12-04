af72a94004c12165459696952d5236590b6f38fd
Alva Swanson <alvasw@protonmail.com>
2023-12-04 16:37:18 +0100

    Migrate oracle_node_app to kotlin dsl

---
+++ oracle_node_app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("bisq.java-library")
+    `application`
+    alias(libs.plugins.shadow)
+}
+
+application {
+    mainClass.set("bisq.oracle_node_app.OracleNodeApp")
+}
+
+dependencies {
+    implementation(project(":persistence"))
+    implementation(project(":security"))
+    implementation(project(":identity"))
+    implementation(project(":bonded_roles"))
+    implementation(project(":oracle_node"))
+    implementation(project(":application"))
+
+    implementation("network:common")
+    implementation("network:network-identity")
+    implementation("network:network")
+
+    implementation(libs.google.gson)
+    implementation(libs.typesafe.config)
+}
+
+tasks {
+    distZip {
+        enabled = false
+    }
+
+    distTar {
+        enabled = false
+    }
+
+    shadowDistZip {
+        enabled = false
+    }
+
+    shadowDistTar {
+        enabled = false
+    }
+
+    shadowJar {
+        enabled = false
+    }
+}
