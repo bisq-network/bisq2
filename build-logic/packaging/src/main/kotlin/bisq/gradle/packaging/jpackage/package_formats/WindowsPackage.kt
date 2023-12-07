08fd044156ceebe99d195b18645641e6f63b71f5
Alva Swanson <alvasw@protonmail.com>
2023-12-07 13:10:43 +0100

    packaging: Add Windows resources directory

--- build-logic/packaging/src/main/kotlin/bisq/gradle/packaging/jpackage/package_formats/WindowsPackage.kt
+++ build-logic/packaging/src/main/kotlin/bisq/gradle/packaging/jpackage/package_formats/WindowsPackage.kt
@@ -7,6 +7,7 @@ class WindowsPackage(private val resourcesPath: Path) : JPackagePackageFormatCon
 
     override fun createArgumentsForJPackage(packageFormat: PackageFormat): List<String> =
             mutableListOf(
+                    "--resource-dir", resourcesPath.toAbsolutePath().toString(),
                     "--icon", resourcesPath.resolve("Bisq2.ico").toAbsolutePath().toString(),
 
                     "--win-dir-chooser",
