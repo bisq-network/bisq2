bf5ba7a37400a9cce22098a5c4a5d13bc1b10d1a
djing chan <djing-chan-8833@tuta.io>
2023-12-15 14:38:16 +0700

    Use KeyBundle instead of KeyPair in RestApi

--- apps/rest-api-app/src/main/java/bisq/rest_api/endpoints/KeyBundleApi.java
+++ apps/rest-api-app/src/main/java/bisq/rest_api/endpoints/KeyBundleApi.java
@@ -19,9 +19,10 @@ package bisq.rest_api.endpoints;
 
 import bisq.rest_api.JaxRsApplication;
 import bisq.rest_api.RestApiApplicationService;
-import bisq.rest_api.dto.KeyPairDto;
+import bisq.rest_api.dto.KeyBundleDto;
 import bisq.rest_api.error.StatusException;
 import bisq.security.KeyBundleService;
+import bisq.security.keys.KeyBundle;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -38,60 +39,58 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
 
-import java.security.KeyPair;
-
 @Slf4j
-@Path("/key-pair")
+@Path("/key-bundle")
 @Produces(MediaType.APPLICATION_JSON)
-@Tag(name = "Key Pair API")
-public class KeyPairApi {
-    public static final String DESC_KEY_ID = "The ID for identifying the key which we look up or create in case it does not exist.";
+@Tag(name = "Key bundle API")
+public class KeyBundleApi {
+    public static final String DESC_KEY_ID = "The ID for identifying the key bundle which we look up or create in case it does not exist.";
 
     private final KeyBundleService keyBundleService;
 
-    public KeyPairApi(@Context Application application) {
+    public KeyBundleApi(@Context Application application) {
         RestApiApplicationService applicationService = ((JaxRsApplication) application).getApplicationService().get();
         keyBundleService = applicationService.getSecurityService().getKeyBundleService();
     }
 
     /**
      * @param keyId The ID for identifying the key which we look up or create in case it does not exist.
-     * @return The key pair.
+     * @return The key bundle.
      */
-    @Operation(description = "find the key pair for given ID")
-    @ApiResponse(responseCode = "200", description = "the created or existing key pair for the given key-id",
+    @Operation(description = "find the key bundle for given ID")
+    @ApiResponse(responseCode = "200", description = "the created or existing key bundle for the given key-id",
             content = {
                     @Content(
                             mediaType = MediaType.APPLICATION_JSON,
-                            schema = @Schema(implementation = KeyPairDto.class)
+                            schema = @Schema(implementation = KeyBundleDto.class)
                     )}
     )
     @GET
     @Path("get-or-create/{key-id}")
-    public KeyPairDto getOrCreateKeyPair(
+    public KeyBundleDto getOrCreateKeyBundle(
             @Parameter(description = DESC_KEY_ID) @PathParam("key-id") String keyId) {
-        KeyPair keyPair = keyBundleService.getOrCreateKeyPair(keyId);
-        return KeyPairDto.from(keyPair);
+        KeyBundle keyBundle = keyBundleService.getOrCreateKeyBundle(keyId);
+        return KeyBundleDto.from(keyBundle);
     }
 
     /**
      * @param keyId The ID for identifying the key which we look up.
-     * @return The key pair if a key pair with that keyId exist, otherwise null.
+     * @return The key bundle if a key bundle with that keyId exist, otherwise null.
      */
 
-    @Operation(summary = "find the key pair for given ID")
+    @Operation(summary = "find the key bundle for given ID")
     @ApiResponse(responseCode = "404", description = "key-id was not found")
-    @ApiResponse(responseCode = "200", description = "key pair for the key-id",
+    @ApiResponse(responseCode = "200", description = "key bundle for the key-id",
             content = {
                     @Content(
                             mediaType = "application/json",
-                            schema = @Schema(implementation = KeyPairDto.class)
+                            schema = @Schema(implementation = KeyBundleDto.class)
                     )}
     )
     @GET
     @Path("get/{key-id}")
-    public KeyPairDto findKeyPair(@Parameter(description = DESC_KEY_ID) @PathParam("key-id") String keyId) {
-        return KeyPairDto.from(keyBundleService.findKeyPair(keyId)
-                .orElseThrow(() -> new StatusException(Response.Status.NOT_FOUND, "Could not find the key pair for ID " + keyId)));
+    public KeyBundleDto findKeyBundle(@Parameter(description = DESC_KEY_ID) @PathParam("key-id") String keyId) {
+        return KeyBundleDto.from(keyBundleService.findKeyBundle(keyId)
+                .orElseThrow(() -> new StatusException(Response.Status.NOT_FOUND, "Could not find the key bundle for ID " + keyId)));
     }
 }
