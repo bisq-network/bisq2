64947b985767630bc7d7a5c2876512d700ee1540
axpoems <145597137+axpoems@users.noreply.github.com>
2023-12-29 14:25:29 +0100

    Improve empty private chats placeholder text

--- apps/desktop/desktop/src/main/java/bisq/desktop/main/content/components/ChatMessagesComponent.java
+++ apps/desktop/desktop/src/main/java/bisq/desktop/main/content/components/ChatMessagesComponent.java
@@ -594,11 +594,17 @@ public class ChatMessagesComponent {
             leaveChannelButton = createAndGetChatButton(Res.get("chat.leave"), 120);
             leaveChannelButton.getStyleClass().add("outlined-button");
 
-            Label noChatsLabel = new Label(Res.get("chat.private.messagebox.noChats.description"));
-            noChatsLabel.getStyleClass().add("chat-container-placeholder-text");
-            noChatsLabel.setTextAlignment(TextAlignment.CENTER);
-            emptyMessageList = new VBox(noChatsLabel);
+            Label noChatsPlaceholderTitle = new Label(Res.get("chat.private.messagebox.noChats.placeholder.title"));
+            noChatsPlaceholderTitle.getStyleClass().add("large-text");
+            noChatsPlaceholderTitle.setTextAlignment(TextAlignment.CENTER);
+
+            Label noChatsPlaceholderDescription = new Label(Res.get("chat.private.messagebox.noChats.placeholder.description"));
+            noChatsPlaceholderDescription.getStyleClass().add("normal-text");
+            noChatsPlaceholderDescription.setTextAlignment(TextAlignment.CENTER);
+
+            emptyMessageList = new VBox(10, noChatsPlaceholderTitle, noChatsPlaceholderDescription);
             emptyMessageList.setAlignment(Pos.CENTER);
+            emptyMessageList.getStyleClass().add("chat-container-placeholder-text");
             VBox.setVgrow(emptyMessageList, Priority.ALWAYS);
 
             createOfferButton = new Button(Res.get("offer.createOffer"));
