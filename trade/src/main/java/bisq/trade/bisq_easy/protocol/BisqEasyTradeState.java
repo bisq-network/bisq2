08874eb4b7c3912c65332e9cb6628fc729a98f02
axpoems <145597137+axpoems@users.noreply.github.com>
2024-01-24 09:01:25 +0100

    Introduce new BisqEasy trade states

--- trade/src/main/java/bisq/trade/bisq_easy/protocol/BisqEasyTradeState.java
+++ trade/src/main/java/bisq/trade/bisq_easy/protocol/BisqEasyTradeState.java
@@ -29,14 +29,79 @@ public enum BisqEasyTradeState implements State {
     // Take offer
     TAKER_SENT_TAKE_OFFER_REQUEST,
     MAKER_SENT_TAKE_OFFER_RESPONSE,
-    TAKER_RECEIVED_TAKE_OFFER_RESPONSE,
 
+
+    TAKER_RECEIVED_TAKE_OFFER_RESPONSE, // TODO: REMOVE
+
+    // BUYER AS TAKER *****************************/
+    // Branch 1: Buyer received take offer response first
+    TAKER_RECEIVED_TAKE_OFFER_RESPONSE__BUYER_DID_NOT_SENT_BTC_ADDRESS__BUYER_DID_NOT_RECEIVED_ACCOUNT_DATA,
+
+        // Branch 1.1.: Buyer sent Btc address first
+        TAKER_RECEIVED_TAKE_OFFER_RESPONSE__BUYER_SENT_BTC_ADDRESS__BUYER_DID_NOT_RECEIVED_ACCOUNT_DATA,
+
+        // Branch 1.2.: Buyer received account data first
+        TAKER_RECEIVED_TAKE_OFFER_RESPONSE__BUYER_DID_NOT_SENT_BTC_ADDRESS__BUYER_RECEIVED_ACCOUNT_DATA,
+
+    // Branch 2: Buyer sent Btc address first
+    TAKER_DID_NOT_RECEIVED_TAKE_OFFER_RESPONSE__BUYER_SENT_BTC_ADDRESS__BUYER_DID_NOT_RECEIVED_ACCOUNT_DATA,
+    //TAKER_RECEIVED_TAKE_OFFER_RESPONSE__BUYER_SENT_BTC_ADDRESS__BUYER_DID_NOT_RECEIVED_ACCOUNT_DATA,
+
+    // Unique final converging step (all three states have been completed)
+    TAKER_RECEIVED_TAKE_OFFER_RESPONSE__BUYER_SENT_BTC_ADDRESS__BUYER_RECEIVED_ACCOUNT_DATA,
+    // *********************************************/
+
+
+    // SELLER AS TAKER *****************************/
+    // Branch 1: Seller received take offer response first
+    TAKER_RECEIVED_TAKE_OFFER_RESPONSE__SELLER_DID_NOT_SENT_ACCOUNT_DATA__SELLER_DID_NOT_RECEIVED_BTC_ADDRESS,
+
+        // Branch 1.1.: Seller sent account data first
+        TAKER_RECEIVED_TAKE_OFFER_RESPONSE__SELLER_SENT_ACCOUNT_DATA__SELLER_DID_NOT_RECEIVED_BTC_ADDRESS,
+
+        // Branch 1.2.: Buyer received account data first
+        TAKER_RECEIVED_TAKE_OFFER_RESPONSE__SELLER_DID_NOT_SENT_ACCOUNT_DATA__SELLER_RECEIVED_BTC_ADDRESS,
+
+    // Branch 2: Seller sent account data first
+    TAKER_DID_NOT_RECEIVED_TAKE_OFFER_RESPONSE__SELLER_SENT_ACCOUNT_DATA__SELLER_DID_NOT_RECEIVED_BTC_ADDRESS,
+    //TAKER_RECEIVED_TAKE_OFFER_RESPONSE__SELLER_SENT_ACCOUNT_DATA__SELLER_DID_NOT_RECEIVED_BTC_ADDRESS,
+
+    // Unique final converging step (all three states have been completed)
+    TAKER_RECEIVED_TAKE_OFFER_RESPONSE__SELLER_SENT_ACCOUNT_DATA__SELLER_RECEIVED_BTC_ADDRESS,
+    // *********************************************/
+
+
+    // SELLER AS MAKER *****************************/
+    // Branch 1: Seller sent account data first
+    SELLER_SENT_ACCOUNT_DATA__SELLER_DID_NOT_RECEIVED_BTC_ADDRESS,
+
+    // Branch 2: Seller received btc address first
+    SELLER_DID_NOT_SENT_ACCOUNT_DATA__SELLER_RECEIVED_BTC_ADDRESS,
+
+    // Unique final converging step (the two states have been completed)
+    SELLER_SENT_ACCOUNT_DATA__SELLER_RECEIVED_BTC_ADDRESS,
+    // *********************************************/
+
+
+    // BUYER AS MAKER *****************************/
+    // Branch 1: Buyer sends btc address first
+    BUYER_SENT_BTC_ADDRESS__BUYER_DID_NOT_RECEIVED_ACCOUNT_DATA,
+
+    // Branch 2: Buyer receives account data first
+    BUYER_DID_NOT_SENT_BTC_ADDRESS__BUYER_RECEIVED_ACCOUNT_DATA,
+
+    // Unique final converging step (the two states have been completed)
+    BUYER_SENT_BTC_ADDRESS__BUYER_RECEIVED_ACCOUNT_DATA,
+    // *********************************************/
+
+
+    // TODO: Remove
     // Account details
-    // Option 1: Peer starts sending
+    // Branch 1: Peer starts sending
     BUYER_DID_NOT_SEND_BTC_ADDRESS_AND_RECEIVED_ACCOUNT_DATA,
     SELLER_DID_NOT_SEND_ACCOUNT_DATA_AND_RECEIVED_BTC_ADDRESS,
 
-    // Option 2: Self start sending
+    // Branch 2: Self start sending
     BUYER_SENT_BTC_ADDRESS_AND_WAITING_FOR_ACCOUNT_DATA,
     SELLER_SENT_ACCOUNT_DATA_AND_WAITING_FOR_BTC_ADDRESS,
 
