cf656ae72104b55716fccc1ef44a0f941f1fdd58
djing chan <djing-chan-8833@tuta.io>
2024-01-23 23:26:05 -0500

    Change order in enum

--- persistence/src/main/java/bisq/persistence/DbSubDirectory.java
+++ persistence/src/main/java/bisq/persistence/DbSubDirectory.java
@@ -6,9 +6,9 @@ import java.io.File;
 
 @Getter
 public enum DbSubDirectory {
-    SETTINGS("settings"),      // Used for dataStores which persist user setting data
-    CACHE("cache"),            // Used for dataStores which persist data derived from the network
     NETWORK_DB("network_db"),  // Used for dataStores which persist data from the network data storage
+    CACHE("cache"),            // Used for dataStores which persist data derived from the network
+    SETTINGS("settings"),      // Used for dataStores which persist user setting data
     PRIVATE("private"),        // Used for dataStores which persist private user data
     WALLETS("wallets");        // Used for dataStores which persist wallets
 
