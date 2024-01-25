c1ed033747041e15b49f4ba7a54119d2a7b8f023
djing chan <djing-chan-8833@tuta.io>
2024-01-24 21:17:12 -0500

    Add myPreferredAuthorizationTypes config, Add AuthorizationType, add/change entries in Feature

--- network/network/src/main/java/bisq/network/p2p/node/authorization/AuthorizationType.java
+++ network/network/src/main/java/bisq/network/p2p/node/authorization/AuthorizationType.java
@@ -1,4 +1,4 @@
-package bisq.network.p2p.node;
+package bisq.network.p2p.node.authorization;
 
 import bisq.common.proto.ProtoEnum;
 import bisq.common.util.ProtobufUtils;
@@ -7,19 +7,17 @@ import bisq.common.util.ProtobufUtils;
  * Features are used to signal which features the node supports. Those might be variations of different implementations
  * like HashCash or EquiHash algorithms for proof of work, or different implementations for requesting inventory data.
  */
-public enum Feature implements ProtoEnum {
-    INVENTORY_HASH_SET,
-    INVENTORY_MINI_SKETCH,
-    AUTHORIZATION_TOKEN_POW_HASH_CASH,
-    AUTHORIZATION_TOKEN_POW_EQUI_HASH,
-    AUTHORIZATION_TOKEN_BURNED_BSQ;
+public enum AuthorizationType implements ProtoEnum {
+    POW_HASH_CASH,
+    POW_EQUI_HASH,
+    BURNED_BSQ;
 
     @Override
     public bisq.network.protobuf.Feature toProto() {
         return bisq.network.protobuf.Feature.valueOf(getProtobufEnumPrefix() + name());
     }
 
-    public static Feature fromProto(bisq.network.protobuf.Feature proto) {
-        return ProtobufUtils.enumFromProto(Feature.class, proto.name());
+    public static AuthorizationType fromProto(bisq.network.protobuf.Feature proto) {
+        return ProtobufUtils.enumFromProto(AuthorizationType.class, proto.name());
     }
 }
