18b9231aa2dc7b2772eeecd28838bd5ecc8812e9
HenrikJannsen <boilingfrog@gmx.com>
2024-03-26 13:04:15 +0700

    Add temp. default getBuilder impl for development

--- common/src/main/java/bisq/common/proto/Proto.java
+++ common/src/main/java/bisq/common/proto/Proto.java
@@ -37,7 +37,11 @@ import java.util.stream.Collectors;
  * If a map is needed we can use the TreeMap as it provides a deterministic order.
  */
 public interface Proto {
-    Message.Builder getBuilder();
+    //Message.Builder getBuilder();
+    // TODO temp for dev
+    default Message.Builder getBuilder() {
+        return null;
+    }
 
     default Message toProto() {
         return getBuilder().build();
