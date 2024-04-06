539d07a5627e077fd39e2ccec5d5eb6048c6bfd7
HenrikJannsen <boilingfrog@gmx.com>
2024-04-06 13:00:32 +0700

    Reformat (force re-run of CI)

--- trade/src/main/java/bisq/trade/bisq_easy/protocol/events/BisqEasyFsmErrorEventHandler.java
+++ trade/src/main/java/bisq/trade/bisq_easy/protocol/events/BisqEasyFsmErrorEventHandler.java
@@ -45,20 +45,20 @@ public class BisqEasyFsmErrorEventHandler extends SendTradeMessageHandler<BisqEa
         commitToModel(ExceptionUtil.getRootCauseMessage(fsmException),
                 ExceptionUtil.getStackTraceAsString(fsmException));
 
-            // We do not send the error message to the peer as there is risk that private data could be leaked.
-            // Instead, we send the stack of causes as error message.
-            // We might send more meaningful error messages in the future.
+        // We do not send the error message to the peer as there is risk that private data could be leaked.
+        // Instead, we send the stack of causes as error message.
+        // We might send more meaningful error messages in the future.
         String errorMessage = truncate(ExceptionUtil.getCauseStackClassNames(fsmException), MAX_LENGTH_ERROR_MESSAGE);
         String stackTrace = truncate(ExceptionUtil.getSafeStackTraceAsString(fsmException), MAX_LENGTH_STACKTRACE);
-            log.warn("We send the cause stack and stackTrace to our peer.\n" +
-                    "errorMessage={}\nstackTrace={}", errorMessage, stackTrace);
-            sendMessage(new BisqEasyReportErrorMessage(createUid(),
-                    trade.getId(),
-                    trade.getProtocolVersion(),
-                    trade.getMyIdentity().getNetworkId(),
-                    trade.getPeer().getNetworkId(),
-                    errorMessage,
-                    stackTrace));
+        log.warn("We send the cause stack and stackTrace to our peer.\n" +
+                "errorMessage={}\nstackTrace={}", errorMessage, stackTrace);
+        sendMessage(new BisqEasyReportErrorMessage(createUid(),
+                trade.getId(),
+                trade.getProtocolVersion(),
+                trade.getMyIdentity().getNetworkId(),
+                trade.getPeer().getNetworkId(),
+                errorMessage,
+                stackTrace));
     }
 
     private void commitToModel(String errorMessage, String errorStackTrace) {
