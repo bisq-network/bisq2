2d31b2053015f267c2351c64b58413fb18b88a5a
HenrikJannsen <boilingfrog@gmx.com>
2024-04-21 22:12:24 +0700

    Add ExcludeForHash to AckMessage and change metadata

--- network/network/src/main/java/bisq/network/p2p/services/confidential/ack/AckMessage.java
+++ network/network/src/main/java/bisq/network/p2p/services/confidential/ack/AckMessage.java
@@ -1,5 +1,6 @@
 package bisq.network.p2p.services.confidential.ack;
 
+import bisq.common.annotation.ExcludeForHash;
 import bisq.common.validation.NetworkDataValidation;
 import bisq.network.p2p.message.Response;
 import bisq.network.p2p.services.data.storage.MetaData;
@@ -18,7 +19,8 @@ import static bisq.network.p2p.services.data.storage.MetaData.*;
 @EqualsAndHashCode
 @ToString
 public final class AckMessage implements MailboxMessage, Response {
-    private final MetaData metaData = new MetaData(TTL_2_DAYS, LOW_PRIORITY, getClass().getSimpleName(), MAX_MAP_SIZE_100);
+    @ExcludeForHash
+    private final MetaData metaData = new MetaData(TTL_2_DAYS, LOW_PRIORITY, getClass().getSimpleName(), MAX_MAP_SIZE_10_000);
 
     private final String id;
 
