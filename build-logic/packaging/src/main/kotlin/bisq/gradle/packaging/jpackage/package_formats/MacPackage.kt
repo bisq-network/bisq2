2694a46c03399531ff9a310394e39ce087b3b3a6
Alva Swanson <alvasw@protonmail.com>
2024-06-24 17:09:34 +0000

    MacPackage: Add Bisq 1 icon support

--- build-logic/packaging/src/main/kotlin/bisq/gradle/packaging/jpackage/package_formats/MacPackage.kt
+++ build-logic/packaging/src/main/kotlin/bisq/gradle/packaging/jpackage/package_formats/MacPackage.kt
@@ -5,10 +5,13 @@ import java.nio.file.Path
 class MacPackage(private val resourcesPath: Path, private val appName: String) : JPackagePackageFormatConfigs {
     override val packageFormats = setOf(PackageFormat.DMG)
 
-    override fun createArgumentsForJPackage(packageFormat: PackageFormat): List<String> =
-            mutableListOf(
-                    "--resource-dir", resourcesPath.toAbsolutePath().toString(),
-                    "--mac-package-name", appName,
-                    "--icon", resourcesPath.resolve("Bisq2.icns").toAbsolutePath().toString(),
-            )
+    override fun createArgumentsForJPackage(packageFormat: PackageFormat): List<String> {
+        // App name is "Bisq" or "Bisq 2"
+        val iconFileName = "${appName.replace(" ", "")}.icns"
+        return mutableListOf(
+            "--resource-dir", resourcesPath.toAbsolutePath().toString(),
+            "--mac-package-name", appName,
+            "--icon", resourcesPath.resolve(iconFileName).toAbsolutePath().toString(),
+        )
+    }
 }
