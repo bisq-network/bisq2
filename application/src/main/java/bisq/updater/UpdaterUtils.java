c40e51540dd86c1e4abb678da6c6026b3d97f187
Alva Swanson <alvasw@protonmail.com>
2024-06-24 17:15:34 +0000

    updater: Download os specific app jar

--- application/src/main/java/bisq/updater/UpdaterUtils.java
+++ application/src/main/java/bisq/updater/UpdaterUtils.java
@@ -52,7 +52,7 @@ public class UpdaterUtils {
     }
 
     public static String getJarFileName(String version) {
-        return "desktop-app-" + version + "-all.jar";
+        return "desktop-app-" + version + "-" + System.getProperty("os.name").toLowerCase() + "-all.jar";
     }
 
     public static Optional<String> readVersionFromVersionFile(String userDataDir) {
