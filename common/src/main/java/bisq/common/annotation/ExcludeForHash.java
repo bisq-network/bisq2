6ae56456655f8a90261319964561e535ad9f618f
HenrikJannsen <boilingfrog@gmx.com>
2024-06-30 18:45:33 +0700

    Add excludeOnlyInVersions param to ExcludeForHash annotation
    This allows more fine-grained control when the ExcludeForHash is used.

--- common/src/main/java/bisq/common/annotation/ExcludeForHash.java
+++ common/src/main/java/bisq/common/annotation/ExcludeForHash.java
@@ -9,4 +9,6 @@ import java.lang.annotation.Target;
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExcludeForHash {
+    // If not empty, the field will only be excluded if at least one version matches the value returned from getVersion()
+    int[] excludeOnlyInVersions() default {};
 }
