5ca07565fa1d9865bfe394f05f47f21a34c417aa
HenrikJannsen <boilingfrog@gmx.com>
2024-08-25 13:22:53 +0700

    Update tor.version to 13.5.2. Adopt change in name patter to put version after platform name.
    The previous version did fail with the signature checks.

--- build-logic/tor-binary/src/main/kotlin/bisq/gradle/tor_binary/TorBinaryUrlProvider.kt
+++ build-logic/tor-binary/src/main/kotlin/bisq/gradle/tor_binary/TorBinaryUrlProvider.kt
@@ -7,11 +7,11 @@ class TorBinaryUrlProvider(private val version: String) : PerOsUrlProvider {
         get() = "https://archive.torproject.org/tor-package-archive/torbrowser/$version/"
 
     override val linuxUrl: String
-        get() = "tor-expert-bundle-$version-linux-x86_64.tar.gz"
+        get() = "tor-expert-bundle-linux-x86_64-$version.tar.gz"
 
     override val macOsUrl: String
-        get() = "tor-expert-bundle-$version-macos-x86_64.tar.gz"
+        get() = "tor-expert-bundle-macos-x86_64-$version.tar.gz"
 
     override val windowsUrl: String
-        get() = "tor-expert-bundle-$version-windows-x86_64.tar.gz"
+        get() = "tor-expert-bundle-windows-x86_64-$version.tar.gz"
 }
