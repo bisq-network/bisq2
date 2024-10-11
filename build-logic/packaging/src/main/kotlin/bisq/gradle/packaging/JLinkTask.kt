0f7be904b456eba45c61b4b102efe482f8f44b7a
Alejandro García <117378669+alejandrogarcia83@users.noreply.github.com>
2024-10-11 19:19:37 +0000

    Revert "Use JRE instead of JDK Desktop Binary"

--- build-logic/packaging/src/main/kotlin/bisq/gradle/packaging/JLinkTask.kt
+++ build-logic/packaging/src/main/kotlin/bisq/gradle/packaging/JLinkTask.kt
@@ -3,18 +3,14 @@ package bisq.gradle.packaging
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.tasks.*
-import java.io.BufferedReader
-import java.io.InputStreamReader
-
-
-//import java.util.regex.Pattern
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+import java.util.concurrent.TimeUnit
 
 abstract class JLinkTask : DefaultTask() {
-
-//    companion object {
-//        val pattern: Pattern = Pattern.compile(".*\\b(java\\..*|javax\\..*)\\b.*\$")
-//    }
 
     @get:InputDirectory
     abstract val jdkDirectory: DirectoryProperty
@@ -31,105 +27,46 @@ abstract class JLinkTask : DefaultTask() {
 
     @TaskAction
     fun run() {
-        // Ensure the output directory is clean
+        // jlink expects non-existent output directory
         val outputDirectoryFile = outputDirectory.asFile.get()
-        if (outputDirectoryFile.exists() && outputDirectoryFile.listFiles()?.isNotEmpty() == true) {
-            // In case you want to run it manually, this will stop the plugin from running the task
-            logger.lifecycle("custom jre already created, skipping - see: ${jdkDirectory.get()}")
-        } else {
-            if (!outputDirectoryFile.canWrite()) {
-                throw IllegalStateException("Output directory is not writable: ${outputDirectoryFile.absolutePath}")
-            }
-            outputDirectoryFile.deleteRecursively()
+        outputDirectoryFile.deleteRecursively()
 
-            logger.lifecycle("JDK path: ${jdkDirectory.get()}")
-
-            // Construct the jlink command
-            val jLinkPath = jdkDirectory.asFile.get().toPath().resolve("bin").resolve("jlink")
-            val processBuilder = ProcessBuilder(
+        val jLinkPath = jdkDirectory.asFile.get().toPath().resolve("bin").resolve("jlink")
+        val processBuilder = ProcessBuilder(
                 jLinkPath.toAbsolutePath().toString(),
-                "--verbose",
+
                 "--add-modules", parseUsedJavaModulesFromJDepsOutput(),
-                "--include-locales=en,cs,de,es,it,pcm,pt-BR",
+
                 "--strip-native-commands",
                 "--no-header-files",
                 "--no-man-pages",
-                "--compress=2",
-//            "--strip-debug", // makes jlink fail in some platforms like Ubuntu linux
-//            "--add-reads", "java.base=ALL-UNNAMED",
+                "--strip-debug",
+
                 "--output", outputDirectoryFile.absolutePath
-            )
-            logger.lifecycle("Executing jlink command: ${processBuilder.command().joinToString(" ")}")
+        )
 
-            // Add module path if specified
-            if (javaModulesDirectory.isPresent) {
-                val commands = processBuilder.command()
-                commands.add("--module-path")
-                commands.add(javaModulesDirectory.asFile.get().absolutePath)
-                logger.lifecycle("Executing jlink command: ${commands.joinToString(" ")}")
-            }
+        if (javaModulesDirectory.isPresent) {
+            val commands = processBuilder.command()
+            commands.add("--module-path")
+            commands.add(javaModulesDirectory.asFile.get().absolutePath)
+        }
 
-            logger.lifecycle("Preparing process builder..")
-            processBuilder.inheritIO()
+        processBuilder.inheritIO()
 
-            logger.lifecycle("Starting process builder..")
-            val process = processBuilder.start()
-            if (System.getProperty("os.name").lowercase().contains("windows")) {
-                val reader =
-                    BufferedReader(InputStreamReader(process.inputStream))
-                while ((reader.readLine()) != null) {
-                    // WORKAROUND: Jlink hangs in windows (11pro) when running from this custom plugin
-                    // This buffer reader is needed jus to get it going and won't affect the rest of the platforms
-                }
-            }
-            logger.lifecycle("Reading errors..")
-            val errorOutput = process.errorStream.bufferedReader().readText()
-            val exitCode = process.waitFor()
+        val process = processBuilder.start()
+        process.waitFor(2, TimeUnit.MINUTES)
 
-            if (exitCode != 0) {
-                logger.error("jlink error output:\n$errorOutput")
-                throw IllegalStateException("jlink couldn't create custom runtime. Exit code: $exitCode")
-            }
+        val isSuccess = process.exitValue() == 0
+        if (!isSuccess) {
+            throw IllegalStateException("jlink couldn't create custom runtime.")
         }
     }
 
     private fun parseUsedJavaModulesFromJDepsOutput(): String {
-//        TODO instead of hardcoding he modules process the jdeps report and generate the below so we don't need
-//             to touch this code again in case of needed new jmods in the future
-//            readLines.filter { pattern.matcher(it).matches() }
-//            .flatMap { line -> line.split("->").map { it.trim() } }
-//            .filter { it.startsWith("java.") || it == "bisq.desktop_app_launcher" }
-//            .flatMap { line -> line.split(",").map { it.trim() } }
-//            .distinct()
-//            .joinToString(",")
-//        val readLines = jDepsOutputFile.asFile.get().readLines()
-        val modules = listOf(
-            // base
-            "java.base",
-            "java.datatransfer",
-            "java.desktop",
-            "java.logging",
-            "java.xml",
-            "java.naming",
-            "java.net.http",
-            // security
-            "java.se",
-            "java.security.jgss",
-            // javafx
-            "javafx.base",
-            "javafx.controls",
-            "javafx.fxml",
-            "javafx.graphics",
-            "javafx.media",
-            "javafx.swing",
-//            "javafx.web",
-            // jdk
-            "jdk.unsupported",
-            "jdk.localedata",
-            "jdk.crypto.cryptoki",
-            "jdk.crypto.ec"
-        ).joinToString(",")
-        logger.lifecycle("Modules to be included: $modules")
-        return modules
+        var readLines = jDepsOutputFile.asFile.get().readLines()
+        if (!javaModulesDirectory.isPresent) {
+            readLines = readLines.filter { it.startsWith("java.") }
+        }
+        return readLines.joinToString(",")
     }
 }
