31167e00930479788145dec50c372c2c10881683
Alva Swanson <alvasw@protonmail.com>
2024-10-20 19:46:09 +0000

    Implement MigrationService
    The MigrationService allows us to perform necessary migrations when
    upgrading to a new Bisq version. So far, the only option is to try to
    perform the migrations on each run. The MigrationService runs the
    necessary migrations if the data directory version is below the app
    version.

---
+++ application/src/main/java/bisq/application/migration/Migrator.java
@@ -0,0 +1,32 @@
+package bisq.application.migration;
+
+import bisq.application.migration.migrations.Migration;
+import bisq.common.platform.Version;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+
+@Slf4j
+public class Migrator {
+    private final Version appVersion;
+    private final Path dataDir;
+    private final List<Migration> allMigrations;
+
+    public Migrator(Version appVersion, Path dataDir) {
+        this.appVersion = appVersion;
+        this.dataDir = dataDir;
+        this.allMigrations = Collections.emptyList();
+    }
+
+    public void migrate() {
+        for (Migration migration : allMigrations) {
+            Version migrationVersion = migration.getVersion();
+            if (migrationVersion.belowOrEqual(appVersion)) {
+                log.info("Running {} migrations.", migrationVersion);
+                migration.run(dataDir);
+            }
+        }
+    }
+}
