b2209125fe2c2e346760c7f01db832b384519244
Alva Swanson <alvasw@protonmail.com>
2024-10-21 23:10:54 +0000

    migration: Remove tor files from prior version on Linux
    PR #2938 unbundles Tor from Bisq on Linux and switches to the OSes Tor
    binary. If a user updates from a prior version their data directory will
    contain the old extracted Tor binaries. The migration in this change
    removes the obsolete files.

--- application/src/main/java/bisq/application/migration/migrations/MigrationsForV2_1_2.java
+++ application/src/main/java/bisq/application/migration/migrations/MigrationsForV2_1_2.java
@@ -1,6 +1,8 @@
 package bisq.application.migration.migrations;
 
 import bisq.common.application.ApplicationVersion;
+import bisq.common.file.FileUtils;
+import bisq.common.platform.OS;
 import bisq.common.platform.Version;
 
 import java.io.IOException;
@@ -12,6 +14,11 @@ public class MigrationsForV2_1_2 implements Migration {
     @Override
     public void run(Path dataDir) {
         try {
+            if (OS.isLinux()) {
+                Path torDataDir = dataDir.resolve("tor");
+                FileUtils.deleteFileOrDirectory(torDataDir);
+            }
+
             Path versionFilePath = dataDir.resolve("version");
             Files.writeString(versionFilePath, ApplicationVersion.getVersion().toString());
         } catch (IOException e) {
