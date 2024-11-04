b397b7675345c0070b6c9aa5ef059452e06b42b2
Alva Swanson <alvasw@protonmail.com>
2024-11-04 22:32:48 +0000

    migration: Test migration failures

--- evolution/src/main/java/bisq/evolution/migration/migrations/MigrationFailedException.java
+++ evolution/src/main/java/bisq/evolution/migration/migrations/MigrationFailedException.java
@@ -1,6 +1,10 @@
 package bisq.evolution.migration.migrations;
 
 public class MigrationFailedException extends RuntimeException {
+    public MigrationFailedException(String message) {
+        super(message);
+    }
+
     public MigrationFailedException(Throwable cause) {
         super(cause);
     }
--- evolution/src/test/java/bisq/evolution/migration/MigratorTest.java
+++ evolution/src/test/java/bisq/evolution/migration/MigratorTest.java
@@ -2,6 +2,8 @@ package bisq.evolution.migration;
 
 import bisq.common.application.ApplicationVersion;
 import bisq.common.platform.Version;
+import bisq.evolution.migration.migrations.Migration;
+import bisq.evolution.migration.migrations.MigrationFailedException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -9,6 +11,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,5 +29,31 @@ public class MigratorTest {
 
         String readVersion = Files.readString(dataDir.resolve("version"));
         assertThat(readVersion).isEqualTo(appVersion.toString());
+    }
+
+    @Test
+    void migrationFailure(@TempDir Path dataDir) throws IOException {
+        Path versionFilePath = dataDir.resolve("version");
+        Version dataDirVersion = new Version("2.1.0");
+        Files.writeString(versionFilePath, dataDirVersion.toString());
+
+        Version appVersion = ApplicationVersion.getVersion();
+        var migration = new Migration() {
+            @Override
+            public void run(Path dataDir) {
+                throw new MigrationFailedException("Migration failed.");
+            }
+
+            @Override
+            public Version getVersion() {
+                return new Version("2.1.1");
+            }
+        };
+
+        Migrator migrator = new Migrator(appVersion, dataDir, List.of(migration, migration));
+        migrator.migrate();
+
+        String readVersion = Files.readString(dataDir.resolve("version"));
+        assertThat(readVersion).isEqualTo(dataDirVersion.toString());
     }
 }
