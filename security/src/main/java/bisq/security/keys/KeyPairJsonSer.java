ecd7d12ffd203d25dc64fe3cb93f5f38b822977a
HenrikJannsen <boilingfrog@gmx.com>
2024-11-16 21:29:55 +0700

    Fix deserialisation

--- security/src/main/java/bisq/security/keys/KeyPairJsonSer.java
+++ security/src/main/java/bisq/security/keys/KeyPairJsonSer.java
@@ -19,10 +19,7 @@ package bisq.security.keys;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.*;
 
 import java.io.IOException;
 import java.security.KeyPair;
@@ -36,8 +33,10 @@ public class KeyPairJsonSer {
         public KeyPair deserialize(JsonParser parser, DeserializationContext context) throws IOException {
             var node = parser.getCodec().readTree(parser);
             try {
-                String privateKeyEncoded = node.get("privateKey").toString();
-                String publicKeyEncoded = node.get("publicKey").toString();
+                JsonNode privateKeyJsonNode = (JsonNode) node.get("privateKey");
+                JsonNode publicKeyJsonNode = (JsonNode) node.get("publicKey");
+                String privateKeyEncoded = privateKeyJsonNode.asText();
+                String publicKeyEncoded = publicKeyJsonNode.asText();
                 PrivateKey privateKey = KeyGeneration.generatePrivate(Base64.getDecoder().decode(privateKeyEncoded));
                 PublicKey publicKey = KeyGeneration.generatePublic(Base64.getDecoder().decode(publicKeyEncoded));
                 return new KeyPair(publicKey, privateKey);
