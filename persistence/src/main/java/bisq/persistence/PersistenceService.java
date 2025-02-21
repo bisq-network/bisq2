cd39b3298f0377a5ccdcbf3d0b5e16b48992400f
rodvar <1040902+rodvar@users.noreply.github.com>
2025-02-21 16:34:45 +1100

    - more toList() usages of stream having issues with samsung devices

--- persistence/src/main/java/bisq/persistence/PersistenceService.java
+++ persistence/src/main/java/bisq/persistence/PersistenceService.java
@@ -101,7 +101,7 @@ public class PersistenceService {
         List<CompletableFuture<Void>> list = clients.stream()
                 .map(PersistenceClient::getPersistence)
                 .map(Persistence::pruneBackups)
-                .toList();
+                .collect(Collectors.toList());
         return CompletableFutureUtils.allOf(list).thenApply(l -> null);
     }
 
