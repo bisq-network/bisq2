ab373590a9db3717fbbacd5c957b0e1e84a39109
HenrikJannsen <boilingfrog@gmx.com>
2025-03-21 13:33:32 +0700

    Increase max length of stack trade to 2000 chars

--- apps/desktop/desktop/src/main/java/bisq/desktop/main/content/bisq_easy/trade_wizard/review/TradeWizardReviewController.java
+++ apps/desktop/desktop/src/main/java/bisq/desktop/main/content/bisq_easy/trade_wizard/review/TradeWizardReviewController.java
@@ -460,7 +460,7 @@ public class TradeWizardReviewController implements Controller {
                     if (errorMessage != null) {
                         UIThread.run(() -> new Popup().error(Res.get("bisqEasy.openTrades.failed.popup",
                                         errorMessage,
-                                        StringUtils.truncate(bisqEasyTrade.getErrorStackTrace(), 500)))
+                                        StringUtils.truncate(bisqEasyTrade.getErrorStackTrace(), 2000)))
                                 .show());
                     }
                 }
@@ -469,7 +469,7 @@ public class TradeWizardReviewController implements Controller {
                     if (peersErrorMessage != null) {
                         UIThread.run(() -> new Popup().error(Res.get("bisqEasy.openTrades.failedAtPeer.popup",
                                         peersErrorMessage,
-                                        StringUtils.truncate(bisqEasyTrade.getPeersErrorStackTrace(), 500)))
+                                        StringUtils.truncate(bisqEasyTrade.getPeersErrorStackTrace(), 2000)))
                                 .show());
                     }
                 }
