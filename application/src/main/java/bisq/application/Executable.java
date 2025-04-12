f3cecf576d30a44a15eedd12b2ac78a49044272b
mvp1983 <mvp@forex.ua>
2025-04-12 22:50:17 +0300

    Shutdown gracefully

--- application/src/main/java/bisq/application/Executable.java
+++ application/src/main/java/bisq/application/Executable.java
@@ -69,6 +69,11 @@ public abstract class Executable<T extends ApplicationService> implements ShutDo
 
     protected void exitJvm() {
         log.info("Exiting JVM");
+        //Give a chance for resources to be released
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ignored) {
+        }
         System.exit(PlatformUtils.EXIT_SUCCESS);
     }
 
