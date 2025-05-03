03fb3da31f6ccb08be3394a87f80f46d815e8507
HenrikJannsen <boilingfrog@gmx.com>
2025-05-03 21:04:13 +0700

    Add grpc to gradle file

--- trade/build.gradle.kts
+++ trade/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("bisq.java-library")
     id("bisq.protobuf")
+    id("bisq.grpc")
 }
 
 dependencies {
@@ -23,3 +24,34 @@ dependencies {
 
     implementation(libs.typesafe.config)
 }
+
+val grpcPluginId = "grpc"
+val grpcArtifact = "io.grpc:protoc-gen-grpc-java:1.61.0"
+val protocArtifact = "com.google.protobuf:protoc:4.28.2"
+
+protobuf {
+    protoc {
+        artifact = protocArtifact
+    }
+
+    plugins {
+        // This must be declared before `generateProtoTasks`
+        create(grpcPluginId) {
+            artifact = grpcArtifact
+        }
+    }
+
+    // We refer to the name as a string after it's declared
+    generateProtoTasks {
+        ofSourceSet("main").configureEach {
+            plugins {
+                create(grpcPluginId)
+            }
+        }
+    }
+}
+
+sourceSets["main"].java.srcDirs(
+    "build/generated/source/proto/main/grpc",
+    "build/generated/source/proto/main/java"
+)
