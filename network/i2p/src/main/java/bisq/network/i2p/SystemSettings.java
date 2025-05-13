76433d496a86a73bf444f3b7eb872847e7a1285d
Harsh Sawarkar <harshsawarkar111@gmail.com>
2025-05-13 13:36:51 +0530

    Fixed KeyBundleService to work with default private key
    Signed-off-by: Harsh Sawarkar <harshsawarkar111@gmail.com>

--- network/i2p/src/main/java/bisq/network/i2p/SystemSettings.java
+++ network/i2p/src/main/java/bisq/network/i2p/SystemSettings.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package bisq.network.i2p;
 
 import java.io.File;
@@ -172,11 +189,11 @@ public class SystemSettings {
     }
 
     public static File getUserAppDataDir(String groupName, String appName, boolean create) throws IOException {
-        if(getUserAppHomeDir()==null) {
+        if(getUserDataDir(create)==null) {
             return null;
         }
-        File userAppHomeDir = getUserAppHomeDir();
-        File groupDir = new File(userAppHomeDir, groupName);
+        File userDataDir = getUserDataDir(create);
+        File groupDir = new File(userDataDir, groupName);
         if(groupDir.exists() || (create && groupDir.mkdir())) {
             File appDir = new File(groupDir, appName);
             if(appDir.exists() || (create && appDir.mkdir())) {
