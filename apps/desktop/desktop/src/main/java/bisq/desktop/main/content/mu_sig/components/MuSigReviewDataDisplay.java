b93448e23ed68f519be2cad95c4bb621e2260c0d
axpoems <145597137+axpoems@users.noreply.github.com>
2025-06-17 20:02:55 +0200

    Supress warning for unicode

--- apps/desktop/desktop/src/main/java/bisq/desktop/main/content/mu_sig/components/MuSigReviewDataDisplay.java
+++ apps/desktop/desktop/src/main/java/bisq/desktop/main/content/mu_sig/components/MuSigReviewDataDisplay.java
@@ -141,6 +141,7 @@ public class MuSigReviewDataDisplay {
 
     private static class View extends bisq.desktop.common.view.View<HBox, Model, Controller> {
         private static final double HEIGHT = 61;
+        @SuppressWarnings("UnnecessaryUnicodeEscape")
         private static final String DASH_SYMBOL = "\u2013"; // Unicode for "–"
 
         private final Triple<Text, Label, VBox> direction, paymentMethod;
