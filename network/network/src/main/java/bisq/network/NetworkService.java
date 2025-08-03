a7f5a58af76fdf5305fae87188059d3cd28dc4f0
HenrikJannsen <boilingfrog@gmx.com>
2025-08-03 23:01:17 +0700

    Remove NETWORK_IO_POOL

--- network/network/src/main/java/bisq/network/NetworkService.java
+++ network/network/src/main/java/bisq/network/NetworkService.java
@@ -95,7 +95,6 @@ import static java.util.concurrent.CompletableFuture.supplyAsync;
  */
 @Slf4j
 public class NetworkService implements PersistenceClient<NetworkServiceStore>, Service {
-    public static final ExecutorService NETWORK_IO_POOL = ExecutorFactory.newCachedThreadPool("NetworkService.IO", 5, 3000, 5);
     public static final ExecutorService DISPATCHER = ExecutorFactory.newSingleThreadExecutor("NetworkService.dispatcher");
 
     @Getter
