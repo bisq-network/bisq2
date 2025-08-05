582047af48f49a207d3b2c76488f3a24d0b86dd8
HenrikJannsen <boilingfrog@gmx.com>
2025-08-06 00:04:37 +0700

    Add iteration of subprojects for build and clean

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -16,6 +16,12 @@ tasks.named("clean") {
     dependsOn(subprojects.map { it.tasks.named("clean") })
 }
 
+tasks.named("build") {
+    dependsOn(subprojects.map {
+        it.tasks.named("build")
+    })
+}
+
 extensions.findByName("buildScan")?.withGroovyBuilder {
     setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
     setProperty("termsOfServiceAgree", "yes")
