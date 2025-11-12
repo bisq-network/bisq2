69581689958782fcb9ed13a7353083144dd7bdad
axpoems <145597137+axpoems@users.noreply.github.com>
2025-11-12 18:54:46 +0100

    Use WizardOverlay in Musig review create offer

--- apps/desktop/desktop/src/main/java/bisq/desktop/main/content/mu_sig/create_offer/review/MuSigCreateOfferReviewView.java
+++ apps/desktop/desktop/src/main/java/bisq/desktop/main/content/mu_sig/create_offer/review/MuSigCreateOfferReviewView.java
@@ -17,25 +17,20 @@
 
 package bisq.desktop.main.content.mu_sig.create_offer.review;
 
-import bisq.desktop.common.Transitions;
 import bisq.desktop.common.utils.GridPaneUtil;
 import bisq.desktop.common.view.View;
-import bisq.desktop.components.containers.Spacer;
+import bisq.desktop.components.containers.WizardOverlay;
 import bisq.desktop.components.controls.BisqTooltip;
 import bisq.desktop.components.controls.TextFlowUtils;
-import bisq.desktop.main.content.mu_sig.create_offer.MuSigCreateOfferView;
 import bisq.i18n.Res;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.TextAlignment;
 import javafx.scene.text.TextFlow;
 import lombok.extern.slf4j.Slf4j;
 import org.fxmisc.easybind.EasyBind;
@@ -43,14 +38,13 @@ import org.fxmisc.easybind.Subscription;
 
 @Slf4j
 class MuSigCreateOfferReviewView extends View<StackPane, MuSigCreateOfferReviewModel, MuSigCreateOfferReviewController> {
-    private final static int FEEDBACK_WIDTH = 700;
     public static final String DESCRIPTION_STYLE = "trade-wizard-review-description";
     public static final String VALUE_STYLE = "trade-wizard-review-value";
     public static final String DETAILS_STYLE = "trade-wizard-review-details";
 
     private final Label headline, detailsHeadline, priceDetails, priceDescription,
             paymentMethodDescription, paymentMethod, paymentMethodDetails, fee, feeDetails;
-    private final VBox createOfferSuccess;
+    private final WizardOverlay createOfferSuccessOverlay;
     private final Button createOfferSuccessButton;
     private final GridPane gridPane;
     private final TextFlow price;
@@ -139,12 +133,16 @@ class MuSigCreateOfferReviewView extends View<StackPane, MuSigCreateOfferReviewM
 
         // Feedback overlays
         createOfferSuccessButton = new Button(Res.get("bisqEasy.tradeWizard.review.createOfferSuccessButton"));
-        createOfferSuccess = new VBox(20);
-        configCreateOfferSuccess();
+        createOfferSuccessButton.setDefaultButton(true);
+        createOfferSuccessOverlay = new WizardOverlay(root)
+                .info()
+                .headline("bisqEasy.tradeWizard.review.createOfferSuccess.headline")
+                .description("bisqEasy.tradeWizard.review.createOfferSuccess.subTitle")
+                .buttons(createOfferSuccessButton)
+                .build();
 
         StackPane.setMargin(gridPane, new Insets(40));
-        StackPane.setMargin(createOfferSuccess, new Insets(-MuSigCreateOfferView.TOP_PANE_HEIGHT, 0, 0, 0));
-        root.getChildren().addAll(gridPane, createOfferSuccess);
+        root.getChildren().addAll(gridPane, createOfferSuccessOverlay);
     }
 
     @Override
@@ -173,16 +171,10 @@ class MuSigCreateOfferReviewView extends View<StackPane, MuSigCreateOfferReviewM
 
         createOfferSuccessButton.setOnAction(e -> controller.onShowOfferbook());
 
-        showCreateOfferSuccessPin = EasyBind.subscribe(model.getShowCreateOfferSuccess(),
-                show -> {
-                    createOfferSuccess.setVisible(show);
-                    if (show) {
-                        Transitions.blurStrong(gridPane, 0);
-                        Transitions.slideInTop(createOfferSuccess, 450);
-                    } else {
-                        Transitions.removeEffect(gridPane);
-                    }
-                });
+        showCreateOfferSuccessPin = EasyBind.subscribe(model.getShowCreateOfferSuccess(), shouldShow ->
+                createOfferSuccessOverlay.updateOverlayVisibility(gridPane,
+                        shouldShow,
+                        controller::onKeyPressedWhileShowingOverlay));
 
         if (model.isRangeAmount() && model.getMarket().isCrypto()) {
             GridPane.setMargin(reviewDataDisplay, new Insets(0, 0, 45, 0));
@@ -197,43 +189,6 @@ class MuSigCreateOfferReviewView extends View<StackPane, MuSigCreateOfferReviewM
         showCreateOfferSuccessPin.unsubscribe();
         paymentMethod.setTooltip(null);
         paymentMethodDetails.setTooltip(null);
-    }
-
-    private void configCreateOfferSuccess() {
-        VBox contentBox = getFeedbackContentBox();
-
-        createOfferSuccess.setVisible(false);
-        createOfferSuccess.setAlignment(Pos.TOP_CENTER);
-
-        Label headlineLabel = new Label(Res.get("bisqEasy.tradeWizard.review.createOfferSuccess.headline"));
-        headlineLabel.getStyleClass().add("bisq-text-headline-2");
-
-        Label subtitleLabel = new Label(Res.get("bisqEasy.tradeWizard.review.createOfferSuccess.subTitle"));
-        configFeedbackSubtitleLabel(subtitleLabel);
-
-        createOfferSuccessButton.setDefaultButton(true);
-        VBox.setMargin(createOfferSuccessButton, new Insets(10, 0, 0, 0));
-        contentBox.getChildren().addAll(headlineLabel, subtitleLabel, createOfferSuccessButton);
-        createOfferSuccess.getChildren().addAll(contentBox, Spacer.fillVBox());
-    }
-
-    private VBox getFeedbackContentBox() {
-        VBox contentBox = new VBox(20);
-        contentBox.setAlignment(Pos.TOP_CENTER);
-        contentBox.getStyleClass().setAll("trade-wizard-feedback-bg");
-        contentBox.setPadding(new Insets(30));
-        contentBox.setMaxWidth(FEEDBACK_WIDTH);
-        return contentBox;
-    }
-
-    private void configFeedbackSubtitleLabel(Label subtitleLabel) {
-        subtitleLabel.setTextAlignment(TextAlignment.CENTER);
-        subtitleLabel.setAlignment(Pos.CENTER);
-        subtitleLabel.setMinWidth(FEEDBACK_WIDTH - 200);
-        subtitleLabel.setMaxWidth(subtitleLabel.getMinWidth());
-        subtitleLabel.setMinHeight(100);
-        subtitleLabel.setWrapText(true);
-        subtitleLabel.getStyleClass().add("bisq-text-21");
     }
 
     private Region getLine() {
