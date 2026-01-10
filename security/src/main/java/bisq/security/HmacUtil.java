a275ad86d2519f45dce9ff5c2a627d2051a37ea5
HenrikJannsen <boilingfrog@gmx.com>
2026-01-10 11:30:39 +0700

    Add method and change visibility

--- security/src/main/java/bisq/security/HmacUtil.java
+++ security/src/main/java/bisq/security/HmacUtil.java
@@ -21,21 +21,29 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.crypto.Mac;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
 
 @Slf4j
-class HmacUtil {
+public class HmacUtil {
     public static final String HMAC = "HmacSHA256";
 
-    static boolean verifyHmac(byte[] input, SecretKey secretKey, byte[] hmac) throws GeneralSecurityException {
+    public static boolean verifyHmac(byte[] input, SecretKey secretKey, byte[] hmac) throws GeneralSecurityException {
         byte[] hmacTest = createHmac(input, secretKey);
         return Arrays.equals(hmacTest, hmac);
     }
 
-    static byte[] createHmac(byte[] input, SecretKey secretKey) throws GeneralSecurityException {
+    public static byte[] createHmac(byte[] input, SecretKey secretKey) throws GeneralSecurityException {
         Mac mac = Mac.getInstance(HMAC);
         mac.init(secretKey);
         return mac.doFinal(input);
+    }
+
+    public static SecretKeySpec createHmacKeySpec(byte[] key) {
+        if (key.length < 32) {
+            throw new IllegalArgumentException("HMAC key must be at least 32 bytes");
+        }
+        return new SecretKeySpec(key, HmacUtil.HMAC);
     }
 }
