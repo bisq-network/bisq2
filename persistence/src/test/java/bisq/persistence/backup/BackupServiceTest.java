598c7fa3d0e45615031d15c4e37bcb2e6931b33a
rodvar <1040902+rodvar@users.noreply.github.com>
2026-01-16 15:40:44 +0800

    Remove RestoreService test (not in mobile branch) and add missing @TempDir import

--- persistence/src/test/java/bisq/persistence/backup/BackupServiceTest.java
+++ persistence/src/test/java/bisq/persistence/backup/BackupServiceTest.java
@@ -24,6 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.IOException;
 import java.nio.file.Files;
