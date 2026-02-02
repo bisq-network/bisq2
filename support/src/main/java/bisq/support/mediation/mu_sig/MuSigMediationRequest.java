8e097e935926de09a71696effce6e24491dd219a
KimStrand <231482327+KimStrand@users.noreply.github.com>
2026-02-02 10:13:49 +0100

    Add mu sig mediation support classes.

---
+++ support/src/main/java/bisq/support/mediation/mu_sig/MuSigMediationRequest.java
@@ -0,0 +1,187 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.support.mediation.mu_sig;
+
+import bisq.chat.mu_sig.open_trades.MuSigOpenTradeMessage;
+import bisq.common.proto.ProtoResolver;
+import bisq.common.proto.UnresolvableProtobufMessageException;
+import bisq.common.validation.NetworkDataValidation;
+import bisq.contract.mu_sig.MuSigContract;
+import bisq.network.identity.NetworkId;
+import bisq.network.p2p.message.ExternalNetworkMessage;
+import bisq.network.p2p.services.confidential.ack.AckRequestingMessage;
+import bisq.network.p2p.services.data.storage.MetaData;
+import bisq.network.p2p.services.data.storage.mailbox.MailboxMessage;
+import bisq.user.profile.UserProfile;
+import com.google.protobuf.InvalidProtocolBufferException;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static bisq.network.p2p.services.data.storage.MetaData.HIGH_PRIORITY;
+import static bisq.network.p2p.services.data.storage.MetaData.TTL_10_DAYS;
+import static com.google.common.base.Preconditions.checkArgument;
+
+@Slf4j
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class MuSigMediationRequest implements MailboxMessage, ExternalNetworkMessage, AckRequestingMessage {
+    public static String createMessageId(String tradeId) {
+        // Keep name without MuSig prefix for backward compatibility
+        return "MediationRequest" + "." + tradeId;
+    }
+
+    // MetaData is transient as it will be used indirectly by low level network classes. Only some low level network classes write the metaData to their protobuf representations.
+    private transient final MetaData metaData = new MetaData(TTL_10_DAYS, HIGH_PRIORITY, getClass().getSimpleName());
+    private final MuSigContract contract;
+    private final String tradeId;
+
+    @EqualsAndHashCode.Exclude
+    private final UserProfile requester;
+    @EqualsAndHashCode.Exclude
+    private final UserProfile peer;
+    @EqualsAndHashCode.Exclude
+    private final List<MuSigOpenTradeMessage> chatMessages;
+    @EqualsAndHashCode.Exclude
+    private final Optional<NetworkId> mediatorNetworkId;
+
+    public MuSigMediationRequest(String tradeId,
+                                 MuSigContract contract,
+                                 UserProfile requester,
+                                 UserProfile peer,
+                                 List<MuSigOpenTradeMessage> chatMessages,
+                                 Optional<NetworkId> mediatorNetworkId) {
+        this.tradeId = tradeId;
+        this.contract = contract;
+        this.requester = requester;
+        this.peer = peer;
+        this.chatMessages = maybePrune(chatMessages);
+        this.mediatorNetworkId = mediatorNetworkId;
+
+        // We need to sort deterministically as the data is used in the proof of work check
+        Collections.sort(this.chatMessages);
+
+        verify();
+    }
+
+    @Override
+    public void verify() {
+        NetworkDataValidation.validateTradeId(tradeId);
+        checkArgument(chatMessages.size() < 1000);
+    }
+
+    /**
+     * Keep proto name for backward compatibility
+     */
+
+    @Override
+    public bisq.support.protobuf.MuSigMediationRequest.Builder getValueBuilder(boolean serializeForHash) {
+        bisq.support.protobuf.MuSigMediationRequest.Builder builder = bisq.support.protobuf.MuSigMediationRequest.newBuilder()
+                .setTradeId(tradeId)
+                .setContract(contract.toProto(serializeForHash))
+                .setRequester(requester.toProto(serializeForHash))
+                .setPeer(peer.toProto(serializeForHash))
+                .addAllChatMessages(chatMessages.stream()
+                        .map(e -> e.toValueProto(serializeForHash))
+                        .collect(Collectors.toList()));
+        mediatorNetworkId.ifPresent(mediatorNetworkId -> builder.setMediatorNetworkId(mediatorNetworkId.toProto(serializeForHash)));
+        return builder;
+    }
+
+    @Override
+    public bisq.support.protobuf.MuSigMediationRequest toValueProto(boolean serializeForHash) {
+        return resolveBuilder(this.getValueBuilder(serializeForHash), serializeForHash).build();
+    }
+
+    public static MuSigMediationRequest fromProto(bisq.support.protobuf.MuSigMediationRequest proto) {
+        return new MuSigMediationRequest(proto.getTradeId(),
+                MuSigContract.fromProto(proto.getContract()),
+                UserProfile.fromProto(proto.getRequester()),
+                UserProfile.fromProto(proto.getPeer()),
+                proto.getChatMessagesList().stream()
+                        .map(MuSigOpenTradeMessage::fromProto)
+                        .collect(Collectors.toList()),
+                proto.hasMediatorNetworkId()
+                        ? Optional.of(NetworkId.fromProto(proto.getMediatorNetworkId()))
+                        : Optional.empty());
+    }
+
+
+    /* --------------------------------------------------------------------- */
+    // AckRequestingMessage implementation
+    /* --------------------------------------------------------------------- */
+
+    @Override
+    public String getAckRequestingMessageId() {
+        return createMessageId(tradeId);
+    }
+
+    @Override
+    public NetworkId getSender() {
+        return requester.getNetworkId();
+    }
+
+    @Override
+    public NetworkId getReceiver() {
+        // We access it only after checking with allFieldsValid
+        return mediatorNetworkId.orElseThrow();
+    }
+
+    @Override
+    public boolean allFieldsValid() {
+        return mediatorNetworkId.isPresent();
+    }
+
+    public static ProtoResolver<ExternalNetworkMessage> getNetworkMessageResolver() {
+        return any -> {
+            try {
+                bisq.support.protobuf.MuSigMediationRequest proto = any.unpack(bisq.support.protobuf.MuSigMediationRequest.class);
+                return MuSigMediationRequest.fromProto(proto);
+            } catch (InvalidProtocolBufferException e) {
+                throw new UnresolvableProtobufMessageException(e);
+            }
+        };
+    }
+
+    @Override
+    public double getCostFactor() {
+        return getCostFactor(0.25, 0.5);
+    }
+
+    private List<MuSigOpenTradeMessage> maybePrune(List<MuSigOpenTradeMessage> chatMessages) {
+        StringBuilder sb = new StringBuilder();
+        List<MuSigOpenTradeMessage> result = chatMessages.stream()
+                .filter(message -> {
+                    sb.append(message.getTextOrNA());
+                    return sb.toString().length() < 10_000;
+                })
+                .collect(Collectors.toList());
+        if (result.size() != chatMessages.size()) {
+            log.warn("chatMessages have been pruned as total text size exceeded 10 000 characters. ");
+            log.warn("chatMessages={}", chatMessages);
+        }
+        return result;
+    }
+}
---
+++ support/src/main/java/bisq/support/mediation/mu_sig/MuSigMediatorService.java
@@ -0,0 +1,205 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package bisq.support.mediation.mu_sig;
+
+import bisq.bonded_roles.BondedRoleType;
+import bisq.bonded_roles.BondedRolesService;
+import bisq.bonded_roles.bonded_role.AuthorizedBondedRolesService;
+import bisq.chat.ChatService;
+import bisq.chat.mu_sig.open_trades.MuSigOpenTradeChannel;
+import bisq.chat.mu_sig.open_trades.MuSigOpenTradeChannelService;
+import bisq.chat.mu_sig.open_trades.MuSigOpenTradeMessage;
+import bisq.common.application.Service;
+import bisq.common.observable.collection.ObservableSet;
+import bisq.contract.mu_sig.MuSigContract;
+import bisq.i18n.Res;
+import bisq.network.NetworkService;
+import bisq.network.identity.NetworkIdWithKeyPair;
+import bisq.network.p2p.message.EnvelopePayloadMessage;
+import bisq.network.p2p.services.confidential.ConfidentialMessageService;
+import bisq.persistence.DbSubDirectory;
+import bisq.persistence.Persistence;
+import bisq.persistence.PersistenceService;
+import bisq.persistence.RateLimitedPersistenceClient;
+import bisq.user.UserService;
+import bisq.user.banned.BannedUserService;
+import bisq.user.identity.UserIdentity;
+import bisq.user.identity.UserIdentityService;
+import bisq.user.profile.UserProfile;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+/**
+ * Service used by mediators
+ */
+@Slf4j
+public class MuSigMediatorService extends RateLimitedPersistenceClient<MuSigMediatorStore> implements Service, ConfidentialMessageService.Listener {
+    @Getter
+    private final MuSigMediatorStore persistableStore = new MuSigMediatorStore();
+    @Getter
+    private final Persistence<MuSigMediatorStore> persistence;
+    private final NetworkService networkService;
+    private final UserIdentityService userIdentityService;
+    private final MuSigOpenTradeChannelService muSigOpenTradeChannelService;
+    private final AuthorizedBondedRolesService authorizedBondedRolesService;
+    private final BannedUserService bannedUserService;
+
+    public MuSigMediatorService(PersistenceService persistenceService,
+                                NetworkService networkService,
+                                ChatService chatService,
+                                UserService userService,
+                                BondedRolesService bondedRolesService) {
+        persistence = persistenceService.getOrCreatePersistence(this, DbSubDirectory.PRIVATE, persistableStore);
+        this.networkService = networkService;
+        userIdentityService = userService.getUserIdentityService();
+        bannedUserService = userService.getBannedUserService();
+        authorizedBondedRolesService = bondedRolesService.getAuthorizedBondedRolesService();
+        muSigOpenTradeChannelService = chatService.getMuSigOpenTradeChannelService();
+    }
+
+
+    /* --------------------------------------------------------------------- */
+    // Service
+    /* --------------------------------------------------------------------- */
+
+    @Override
+    public CompletableFuture<Boolean> initialize() {
+        networkService.getConfidentialMessageServices().stream()
+                .flatMap(service -> service.getProcessedEnvelopePayloadMessages().stream())
+                .forEach(this::onMessage);
+        networkService.addConfidentialMessageListener(this);
+        return CompletableFuture.completedFuture(true);
+    }
+
+    @Override
+    public CompletableFuture<Boolean> shutdown() {
+        networkService.removeConfidentialMessageListener(this);
+        return CompletableFuture.completedFuture(true);
+    }
+
+
+    /* --------------------------------------------------------------------- */
+    // ConfidentialMessageService.Listener
+    /* --------------------------------------------------------------------- */
+
+    @Override
+    public void onMessage(EnvelopePayloadMessage envelopePayloadMessage) {
+        if (envelopePayloadMessage instanceof MuSigMediationRequest) {
+            processMediationRequest((MuSigMediationRequest) envelopePayloadMessage);
+        }
+    }
+
+
+    /* --------------------------------------------------------------------- */
+    // API
+    /* --------------------------------------------------------------------- */
+
+    public void closeMediationCase(MuSigMediationCase muSigMediationCase) {
+        if (muSigMediationCase.setClosed(true)) {
+            persist();
+        }
+    }
+
+    public void removeMediationCase(MuSigMediationCase muSigMediationCase) {
+        getMediationCases().remove(muSigMediationCase);
+        persist();
+    }
+
+    public void reOpenMediationCase(MuSigMediationCase muSigMediationCase) {
+        if (muSigMediationCase.setClosed(false)) {
+            persist();
+        }
+    }
+
+    public ObservableSet<MuSigMediationCase> getMediationCases() {
+        return persistableStore.getMuSigMediationCases();
+    }
+
+    public Optional<UserIdentity> findMyMediatorUserIdentity(Optional<UserProfile> mediator) {
+        return findMyMediatorUserIdentities()
+                .filter(e -> mediator.isPresent())
+                .filter(userIdentity -> userIdentity.getUserProfile().getId().equals(mediator.orElseThrow().getId()))
+                .findAny();
+    }
+
+    public Stream<UserIdentity> findMyMediatorUserIdentities() {
+        // If we got banned we still want to show the admin UI
+        return authorizedBondedRolesService.getAuthorizedBondedRoleStream(true)
+                .filter(data -> data.getBondedRoleType() == BondedRoleType.MEDIATOR)
+                .flatMap(data -> userIdentityService.findUserIdentity(data.getProfileId()).stream());
+    }
+
+
+    /* --------------------------------------------------------------------- */
+    // Private
+    /* --------------------------------------------------------------------- */
+
+    private void processMediationRequest(MuSigMediationRequest muSigMediationRequest) {
+        UserProfile requester = muSigMediationRequest.getRequester();
+        if (bannedUserService.isUserProfileBanned(requester)) {
+            log.warn("Message ignored as sender is banned");
+            return;
+        }
+        MuSigContract contract = muSigMediationRequest.getContract();
+        findMyMediatorUserIdentity(contract.getMediator()).ifPresent(myUserIdentity -> {
+            String tradeId = muSigMediationRequest.getTradeId();
+            UserProfile peer = muSigMediationRequest.getPeer();
+            List<MuSigOpenTradeMessage> chatMessages = muSigMediationRequest.getChatMessages();
+            MuSigOpenTradeChannel channel = muSigOpenTradeChannelService.mediatorFindOrCreatesChannel(
+                    tradeId,
+                    myUserIdentity,
+                    requester,
+                    peer
+            );
+
+            muSigOpenTradeChannelService.setIsInMediation(channel, true);
+
+            chatMessages.forEach(chatMessage ->
+                    muSigOpenTradeChannelService.addMessage(chatMessage, channel));
+
+            // We apply the muSigMediationCase after the channel is set up as clients will expect a channel.
+            MuSigMediationCase muSigMediationCase = new MuSigMediationCase(muSigMediationRequest);
+            addNewMediationCase(muSigMediationCase);
+
+            NetworkIdWithKeyPair networkIdWithKeyPair = myUserIdentity.getNetworkIdWithKeyPair();
+
+            // Send to requester
+            networkService.confidentialSend(new MuSigMediatorsResponse(tradeId),
+                    requester.getNetworkId(),
+                    networkIdWithKeyPair);
+            muSigOpenTradeChannelService.addMediatorsResponseMessage(channel, Res.encode("authorizedRole.mediator.message.toRequester"));
+
+            // Send to peer
+            networkService.confidentialSend(new MuSigMediatorsResponse(tradeId),
+                    peer.getNetworkId(),
+                    networkIdWithKeyPair);
+            muSigOpenTradeChannelService.addMediatorsResponseMessage(channel, Res.encode("authorizedRole.mediator.message.toNonRequester"));
+        });
+    }
+
+    private void addNewMediationCase(MuSigMediationCase muSigMediationCase) {
+        getMediationCases().add(muSigMediationCase);
+        persist();
+    }
+}
+
