4c74a56abcbf435ff165753a5933d248a7903cfe
HenrikJannsen <boilingfrog@gmx.com>
2026-02-09 17:08:30 +0700

    Refactor: Use isInstance and cast

--- offer/src/main/java/bisq/offer/options/OfferOptionUtil.java
+++ offer/src/main/java/bisq/offer/options/OfferOptionUtil.java
@@ -57,36 +57,36 @@ public class OfferOptionUtil {
 
     public static Optional<TradeTermsOption> findTradeTermsOption(Collection<OfferOption> offerOptions) {
         return offerOptions.stream()
-                .filter(option -> option instanceof TradeTermsOption)
-                .map(option -> (TradeTermsOption) option)
+                .filter(TradeTermsOption.class::isInstance)
+                .map(TradeTermsOption.class::cast)
                 .findAny();
     }
 
     public static Optional<ReputationOption> findReputationOption(Collection<OfferOption> offerOptions) {
         return offerOptions.stream()
-                .filter(option -> option instanceof ReputationOption)
-                .map(option -> (ReputationOption) option)
+                .filter(ReputationOption.class::isInstance)
+                .map(ReputationOption.class::cast)
                 .findAny();
     }
 
     public static Optional<CollateralOption> findCollateralOption(Collection<OfferOption> offerOptions) {
         return offerOptions.stream()
-                .filter(option -> option instanceof CollateralOption)
-                .map(option -> (CollateralOption) option)
+                .filter(CollateralOption.class::isInstance)
+                .map(CollateralOption.class::cast)
                 .findAny();
     }
 
     public static Optional<FiatPaymentOption> findFiatPaymentOption(Collection<OfferOption> offerOptions) {
         return offerOptions.stream()
-                .filter(option -> option instanceof FiatPaymentOption)
-                .map(option -> (FiatPaymentOption) option)
+                .filter(FiatPaymentOption.class::isInstance)
+                .map(FiatPaymentOption.class::cast)
                 .findAny();
     }
 
     public static Optional<FeeOption> findFeeOption(Collection<OfferOption> offerOptions) {
         return offerOptions.stream()
-                .filter(option -> option instanceof FeeOption)
-                .map(option -> (FeeOption) option)
+                .filter(FeeOption.class::isInstance)
+                .map(FeeOption.class::cast)
                 .findAny();
     }
 
@@ -97,8 +97,8 @@ public class OfferOptionUtil {
 
     public static Set<AccountOption> findAccountOptions(Collection<OfferOption> offerOptions) {
         return offerOptions.stream()
-                .filter(offerOption -> offerOption instanceof AccountOption)
-                .map(offerOption -> (AccountOption) offerOption)
+                .filter(AccountOption.class::isInstance)
+                .map(AccountOption.class::cast)
                 .collect(Collectors.toSet());
     }
 
