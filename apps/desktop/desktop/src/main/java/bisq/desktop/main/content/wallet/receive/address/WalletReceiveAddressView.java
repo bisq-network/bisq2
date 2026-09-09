edff0819a5f6f361e328293f3fff987183411fd5
axpoems <145597137+axpoems@users.noreply.github.com>
2026-09-09 11:49:52 +0200

    Center address bar in wizard

--- apps/desktop/desktop/src/main/java/bisq/desktop/main/content/wallet/receive/address/WalletReceiveAddressView.java
+++ apps/desktop/desktop/src/main/java/bisq/desktop/main/content/wallet/receive/address/WalletReceiveAddressView.java
@@ -71,12 +71,15 @@ public class WalletReceiveAddressView extends View<StackPane, WalletReceiveAddre
 
         createAddressButton = new BisqMenuItem("new-address-green", "new-address-white");
         createAddressButton.setTooltip(Res.get("wallet.receive.createNew"));
+        double addressBarMargin = 30; // for the create address button
+        HBox.setMargin(addressBarHBox, new Insets(0, 0, 0, addressBarMargin));
         HBox addressBarAndCreateButtonHBox = new HBox(20, addressBarHBox, createAddressButton);
         addressBarAndCreateButtonHBox.setAlignment(Pos.CENTER_LEFT);
 
         addressDescriptionLabel = new Label();
         addressDescriptionLabel.getStyleClass().add("address-description");
-        VBox.setMargin(addressDescriptionLabel, new Insets(0, 0, 0, 15));
+        double descriptionMargin = 15;
+        VBox.setMargin(addressDescriptionLabel, new Insets(0, 0, 0, addressBarMargin + descriptionMargin));
         VBox addressVBox = new VBox(2, addressDescriptionLabel, addressBarAndCreateButtonHBox);
 
         // More options
