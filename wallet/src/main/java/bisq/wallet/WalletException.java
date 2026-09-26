2c7aebf5154a7f4ea68ecdad4dfd3798fa2d95d4
balag3 <carbalage@gmail.com>
2026-09-26 09:41:31 +0200

    Map wallet gRPC failures to wallet errors
    Every WalletGrpcClient call now turns a failed gRPC status into a
    WalletException with a reason, keeps the original as cause and logs
    one warning per failed call. Cancelling a returned future still
    cancels the call.
    
    Part of #5037

--- wallet/src/main/java/bisq/wallet/WalletException.java
+++ wallet/src/main/java/bisq/wallet/WalletException.java
@@ -1,15 +1,39 @@
+/*
+ * This file is part of Bisq.
+ *
+ * Bisq is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * Bisq is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package bisq.wallet;
 
 import lombok.Getter;
 
 @Getter
-public class WalletException extends Exception {
-    public WalletException() {
-        super("Wallet Exception");
+public class WalletException extends RuntimeException {
+    public enum Reason {
+        WALLET_NOT_OPEN,
+        WRONG_PASSWORD,
+        DAEMON_UNAVAILABLE,
+        UNSUPPORTED_CALL,
+        CLIENT_NOT_READY,
+        DAEMON_ERROR
     }
 
-    public WalletException(String message) {
-        super(message);
+    private final Reason reason;
+
+    public WalletException(Reason reason, String message, Throwable cause) {
+        super(message, cause);
+        this.reason = reason;
     }
 }
-
